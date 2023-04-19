9b36224f7144b4c09523af7caf623535f4f0268a
Tom Runyon <tom@defenseunicorns.com>
2023-04-19 17:55:06 -0400

    wip
    Signed-off-by: Tom Runyon <tom@defenseunicorns.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# Leapfrog AI
+
+
+## OpenAI Server
+
+The OpenAI API service is hosted and then uses GRPC to talk to the embedding server and the alpaca-lora-7B instance
+
+
+## Create the API Server
+
+models should point to the embedding and the 
---
+++ alpaca/server.py
@@ -0,0 +1,18 @@
+import logging
+
+from model import AlpacaModel as Model
+from simple_ai.api.grpc.completion.server import LanguageModelServicer, serve
+
+if __name__ == "__main__":
+    import argparse
+
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--address", type=str, default="[::]:50051")
+    args = parser.parse_args()
+
+    logging.info(f"Starting gRPC server on {args.address}")
+
+    model_servicer = LanguageModelServicer(model=Model())
+    serve(address=args.address, model_servicer=model_servicer)
