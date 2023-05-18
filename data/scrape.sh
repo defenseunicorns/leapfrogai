585b870884376a4193224ebe49e807b073f64a11
Tom Runyon <tom@defenseunicorns.com>
2023-05-18 06:27:31 -0400

    trimmed down the default data to pull down
    Signed-off-by: Tom Runyon <tom@defenseunicorns.com>

--- data/scrape.sh
+++ data/scrape.sh
@@ -3,5 +3,5 @@
 
 while read url; do
     echo "Getting $url"
-    wget -r -k -l 4 -H -nc --random-wait -e robots=off "$url"
+    wget -r -k -l 2 -nc --random-wait -e robots=off "$url"
 done < seed.txt
