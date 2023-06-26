4c2203373d00bed5708270de19217c1b8039bc88
Tom Runyon <tom@defenseunicorns.com>
2023-06-26 06:58:37 -0400

    Audio work
    Signed-off-by: Tom Runyon <tom@defenseunicorns.com>

--- api/main.go
+++ api/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"github.com/defenseunicorns/leapfrogai/api/config"
+	"github.com/defenseunicorns/leapfrogai/pkg/client/audio"
 	"github.com/defenseunicorns/leapfrogai/pkg/client/generate"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -34,10 +37,192 @@ func main() {
 	r.GET("/models", showModels)
 	r.GET("/models/:model_id", showModel)
 	r.POST("/completions", complete)
+	// r.POST("/audio", audio)
+	r.POST("/audio/transcriptions", audioTranscriptions)
+	r.POST("/audio/translations", audioTranslations)
 	// Define other routes...
 
 	r.Run()
 	// r.Run("0.0.0.0:8080") // Run on default port 8080
+}
+
+func audioTranscriptions(c *gin.Context) {
+	var input openai.AudioRequest
+	if err := c.BindJSON(&input); err != nil {
+		log.Printf("500: Error marshalling input to object: %v\n", err)
+		// Handle error
+		c.JSON(500, err)
+		return
+	}
+	conn := getModelClient(c, input.Model)
+	if conn == nil {
+		return
+	}
+	client := audio.NewAudioClient(conn)
+
+	// get the data from the request
+
+	request := audio.AudioRequest{
+		Model:         input.Model,
+		Data:          []byte{},
+		Prompt:        input.Prompt,
+		Temperature:   input.Temperature,
+		Inputlanguage: input.Language,
+		Format:        string(input.Format),
+	}
+
+	grpcResponse, err := client.Transcribe(context.Background(), &request)
+	if err != nil {
+		log.Printf("500: Error transcribing via %v: %v", input.Model, err)
+		c.JSON(500, err)
+		return
+	}
+
+	response := openai.AudioResponse{
+		Task:     grpcResponse.Task,
+		Language: grpcResponse.Language,
+		Duration: grpcResponse.Duration,
+		Text:     grpcResponse.Text,
+	}
+	response.Segments = make([]struct {
+		ID               int     "json:\"id\""
+		Seek             int     "json:\"seek\""
+		Start            float64 "json:\"start\""
+		End              float64 "json:\"end\""
+		Text             string  "json:\"text\""
+		Tokens           []int   "json:\"tokens\""
+		Temperature      float64 "json:\"temperature\""
+		AvgLogprob       float64 "json:\"avg_logprob\""
+		CompressionRatio float64 "json:\"compression_ratio\""
+		NoSpeechProb     float64 "json:\"no_speech_prob\""
+		Transient        bool    "json:\"transient\""
+	}, 0)
+	for i, seg := range grpcResponse.Segments {
+		response.Segments = append(response.Segments,
+			struct {
+				ID               int     "json:\"id\""
+				Seek             int     "json:\"seek\""
+				Start            float64 "json:\"start\""
+				End              float64 "json:\"end\""
+				Text             string  "json:\"text\""
+				Tokens           []int   "json:\"tokens\""
+				Temperature      float64 "json:\"temperature\""
+				AvgLogprob       float64 "json:\"avg_logprob\""
+				CompressionRatio float64 "json:\"compression_ratio\""
+				NoSpeechProb     float64 "json:\"no_speech_prob\""
+				Transient        bool    "json:\"transient\""
+			}{
+				ID:               int(seg.Id),
+				Seek:             int(seg.Seek),
+				Start:            seg.Start,
+				End:              seg.End,
+				Text:             seg.Text,
+				Temperature:      seg.Temperature,
+				AvgLogprob:       seg.AvgLogprob,
+				CompressionRatio: seg.CompressionRatio,
+				NoSpeechProb:     seg.NoSpeechProb,
+				Transient:        seg.Transient,
+			},
+		)
+		// convert int32 to int
+		tokens := make([]int, len(seg.Tokens))
+		for i, t := range seg.Tokens {
+			tokens[i] = int(t)
+		}
+		response.Segments[i].Tokens = tokens
+	}
+
+	c.JSON(200, response)
+
+}
+
+func audioTranslations(c *gin.Context) {
+	var input openai.AudioRequest
+	if err := c.BindJSON(&input); err != nil {
+		log.Printf("500: Error marshalling input to object: %v\n", err)
+		// Handle error
+		c.JSON(500, err)
+		return
+	}
+	conn := getModelClient(c, input.Model)
+	if conn == nil {
+		return
+	}
+	client := audio.NewAudioClient(conn)
+
+	// get the data from the request
+
+	request := audio.AudioRequest{
+		Model:         input.Model,
+		Data:          []byte{},
+		Prompt:        input.Prompt,
+		Temperature:   input.Temperature,
+		Inputlanguage: input.Language,
+		Format:        string(input.Format),
+	}
+
+	grpcResponse, err := client.Translate(context.Background(), &request)
+	if err != nil {
+		log.Printf("500: Error translating via %v: %v", input.Model, err)
+		c.JSON(500, err)
+		return
+	}
+
+	response := openai.AudioResponse{
+		Task:     grpcResponse.Task,
+		Language: grpcResponse.Language,
+		Duration: grpcResponse.Duration,
+		Text:     grpcResponse.Text,
+	}
+	response.Segments = make([]struct {
+		ID               int     "json:\"id\""
+		Seek             int     "json:\"seek\""
+		Start            float64 "json:\"start\""
+		End              float64 "json:\"end\""
+		Text             string  "json:\"text\""
+		Tokens           []int   "json:\"tokens\""
+		Temperature      float64 "json:\"temperature\""
+		AvgLogprob       float64 "json:\"avg_logprob\""
+		CompressionRatio float64 "json:\"compression_ratio\""
+		NoSpeechProb     float64 "json:\"no_speech_prob\""
+		Transient        bool    "json:\"transient\""
+	}, 0)
+	for i, seg := range grpcResponse.Segments {
+		response.Segments = append(response.Segments,
+			struct {
+				ID               int     "json:\"id\""
+				Seek             int     "json:\"seek\""
+				Start            float64 "json:\"start\""
+				End              float64 "json:\"end\""
+				Text             string  "json:\"text\""
+				Tokens           []int   "json:\"tokens\""
+				Temperature      float64 "json:\"temperature\""
+				AvgLogprob       float64 "json:\"avg_logprob\""
+				CompressionRatio float64 "json:\"compression_ratio\""
+				NoSpeechProb     float64 "json:\"no_speech_prob\""
+				Transient        bool    "json:\"transient\""
+			}{
+				ID:               int(seg.Id),
+				Seek:             int(seg.Seek),
+				Start:            seg.Start,
+				End:              seg.End,
+				Text:             seg.Text,
+				Temperature:      seg.Temperature,
+				AvgLogprob:       seg.AvgLogprob,
+				CompressionRatio: seg.CompressionRatio,
+				NoSpeechProb:     seg.NoSpeechProb,
+				Transient:        seg.Transient,
+			},
+		)
+		// convert int32 to int
+		tokens := make([]int, len(seg.Tokens))
+		for i, t := range seg.Tokens {
+			tokens[i] = int(t)
+		}
+		response.Segments[i].Tokens = tokens
+	}
+
+	c.JSON(200, response)
 }
 
 func showModels(c *gin.Context) {
@@ -64,10 +249,16 @@ func showModel(c *gin.Context) {
 	var m config.Model
 	var ok bool
 	if m, ok = cfg[modelID]; !ok {
-		c.JSON(404, nil)
+		c.JSON(404, fmt.Errorf("model %v not found", modelID))
 		return
 	}
-	c.JSON(200, m)
+	model := openai.Model{
+		OwnedBy:    m.Metadata.OwnedBy,
+		Permission: []openai.Permission{},
+		ID:         modelID,
+	}
+
+	c.JSON(200, model)
 	// Send the response
 }
 
@@ -75,24 +266,35 @@ func complete(c *gin.Context) {
 	// Bind JSON body to a struct with c.BindJSON()
 	var input openai.CompletionRequest
 	if err := c.BindJSON(&input); err != nil {
+		log.Printf("500: Error marshalling input to object: %v\n", err)
 		// Handle error
 		c.JSON(500, err)
 		return
 	}
-	m, ok := cfg[input.Model]
-	if !ok {
-		c.JSON(404, "Model not found")
+	conn := getModelClient(c, input.Model)
+	if conn == nil {
 		return
-	}
-	url := m.Network.URL
-	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
-	if err != nil {
-		c.JSON(500, err)
 	}
 	client := generate.NewCompletionServiceClient(conn)
 	// Implement the completion logic here, using the data from `input`
-	response, err := client.Complete(c.Request.Context(), &generate.CompletionRequest{})
+	response, err := client.Complete(c.Request.Context(), &generate.CompletionRequest{
+		Prompt:      input.Prompt.(string),
+		Suffix:      input.Suffix,
+		MaxTokens:   int32(input.MaxTokens),
+		Temperature: input.Temperature,
+		TopP:        input.TopP,
+		N:           int32(input.N),
+		Stream:      input.Stream,
+		Logprobs:    int32(input.LogProbs),
+		Echo:        input.Echo,
+		// Stop:             input.Stop, // Wrong type here...
+		PresencePenalty:  input.PresencePenalty,
+		FrequencePenalty: input.FrequencyPenalty,
+		BestOf:           int32(input.BestOf),
+		// LogitBias:        input.LogitBias, // Wrong type here
+	})
 	if err != nil {
+		log.Printf("500: Error completing via backend(%v): %v\n", input.Model, err)
 		c.JSON(500, err)
 		return
 	}
@@ -109,4 +311,21 @@ func complete(c *gin.Context) {
 	}
 	c.JSON(200, resp)
 	// Send the response
+}
+
+func getModelClient(c *gin.Context, model string) *grpc.ClientConn {
+	m, ok := cfg[model]
+	if !ok {
+		log.Printf("404: Did not find requested model (%v) in list\n", model)
+		c.JSON(404, "Model not found")
+		return nil
+	}
+	url := m.Network.URL
+	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	if err != nil {
+		log.Printf("500: Error connecting to backend(%v): %v\n", url, err)
+		c.JSON(500, err)
+		return nil
+	}
+	return conn
 }
