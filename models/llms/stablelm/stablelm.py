74680837aafba062a2756cbf146404b384a9cad8
Gerred Dillon <hello@gerred.org>
2023-06-28 11:16:35 -0500

    Prepare for publish to pypi, update all models to be leapfrogai instead of leapfrog

--- models/llms/stablelm/stablelm.py
+++ models/llms/stablelm/stablelm.py
@@ -1,15 +1,28 @@
 import logging
 from typing import List
+
 import torch
 from get_models import MODEL_ID, TOKENIZER_ID
-from leapfrog import (CompletionRequest, CompletionResponse,
-                      CompletionServiceServicer, GrpcContext, serve)
-from transformers import (AutoModelForCausalLM, AutoTokenizer,
-                          StoppingCriteria, StoppingCriteriaList)
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    StoppingCriteria,
+    StoppingCriteriaList,
+)
+
+from leapfrogai import (
+    CompletionRequest,
+    CompletionResponse,
+    CompletionServiceServicer,
+    GrpcContext,
+    serve,
+)
 
 
 class StopOnTokens(StoppingCriteria):
-    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+    def __call__(
+        self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs
+    ) -> bool:
         stop_ids = [50278, 50279, 50277, 1, 0]
         for stop_id in stop_ids:
             if input_ids[0][-1] == stop_id:
@@ -30,25 +43,26 @@ class StableLM(CompletionServiceServicer):
     model = model.to(device)
     logging.info("StableLM Loaded...")
 
-    def Complete(self, request: CompletionRequest, context: GrpcContext) -> CompletionResponse:
-        inputs = self.tokenizer(
-            request.prompt, return_tensors="pt").to(self.device)
+    def Complete(
+        self, request: CompletionRequest, context: GrpcContext
+    ) -> CompletionResponse:
+        inputs = self.tokenizer(request.prompt, return_tensors="pt").to(self.device)
         logging.debug(f"Request:  { request }")
         # error checking for valid params
         tokens = self.model.generate(
-                **inputs,
-                max_new_tokens=request.max_tokens,
-                temperature=request.temperature,
-                # repetition_penalty=request.frequence_penalty,
-                top_p=request.top_p,
-                do_sample=True,
-                pad_token_id=self.tokenizer.eos_token_id,
-                eos_token_id=self.tokenizer.eos_token_id,
-                stopping_criteria=StoppingCriteriaList([StopOnTokens()])
-            )
+            **inputs,
+            max_new_tokens=request.max_tokens,
+            temperature=request.temperature,
+            # repetition_penalty=request.frequence_penalty,
+            top_p=request.top_p,
+            do_sample=True,
+            pad_token_id=self.tokenizer.eos_token_id,
+            eos_token_id=self.tokenizer.eos_token_id,
+            stopping_criteria=StoppingCriteriaList([StopOnTokens()]),
+        )
         logging.debug(f"Response {tokens}")
         # Extract out only the completion tokens
-        completion_tokens = tokens[0][inputs['input_ids'].size(1):]
+        completion_tokens = tokens[0][inputs["input_ids"].size(1) :]
         completion = self.tokenizer.decode(completion_tokens, skip_special_tokens=True)
 
         # response = self.tokenizer.decode(tokens[0], skip_special_tokens=False)
--- models/speech2text/whisper/test.py
+++ models/speech2text/whisper/test.py
@@ -7,19 +7,19 @@ sys.path.append(str(path_root))
 print(sys.path)
 import grpc
 
-import leapfrog
+import leapfrogai
 
 
 def read_iterfile(filepath, chunk_size=1024, language="en"):
-    metadata = leapfrog.AudioMetadata(
+    metadata = leapfrogai.AudioMetadata(
         prompt=None, temperature=0.0, inputlanguage=language
     )
-    yield leapfrog.AudioRequest(metadata=metadata)
+    yield leapfrogai.AudioRequest(metadata=metadata)
     with open(filepath, mode="rb") as f:
         while True:
             chunk = f.read(chunk_size)
             if chunk:
-                entry_request = leapfrog.AudioRequest(chunk_data=chunk)
+                entry_request = leapfrogai.AudioRequest(chunk_data=chunk)
                 yield entry_request
             else:  # The chunk was empty, which means we're at the end of the file
                 return
@@ -28,8 +28,10 @@ def read_iterfile(filepath, chunk_size=1024, language="en"):
 def run():
     # Set up a channel to the server
     with grpc.insecure_channel("localhost:50051") as channel:
-        stub = leapfrog.AudioStub(channel)
-        response: leapfrog.AudioResponse = stub.Transcribe(read_iterfile("geohot.m4a"))
+        stub = leapfrogai.AudioStub(channel)
+        response: leapfrogai.AudioResponse = stub.Transcribe(
+            read_iterfile("geohot.m4a")
+        )
         print(f"Received Transcription: {response.text}")
 
 
