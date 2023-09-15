e9f748e5dbbd5ffdf00e1297be2a8f18446497f6
Gerred Dillon <hello@gerred.org>
2023-09-15 11:26:31 -0500

    move to asyncio grpc

--- leapfrogai/serve.py
+++ leapfrogai/serve.py
@@ -8,17 +8,17 @@ from .embeddings import embeddings_pb2_grpc
 from .completion import completion_pb2_grpc
 from .name import name_pb2_grpc
 
-def serve(o):
 
+async def serve(o):
     # Create a gRPC server
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=3))
+    server = grpc.aio.server(futures.ThreadPoolExecutor(max_workers=40))
 
     if hasattr(o, "ChatComplete"):
         chat_pb2_grpc.add_ChatCompletionServiceServicer_to_server(o, server)
 
     if hasattr(o, "ChatCompleteStream"):
         chat_pb2_grpc.add_ChatCompletionStreamServiceServicer_to_server(o, server)
-    
+
     if hasattr(o, "Complete"):
         completion_pb2_grpc.add_CompletionServiceServicer_to_server(o, server)
 
@@ -37,8 +37,8 @@ def serve(o):
     # Listen on port 50051
     print("Starting server. Listening on port 50051.")
     server.add_insecure_port("[::]:50051")
-    server.start()
+    await server.start()
 
     # block the thread until the server terminates...without using async to await the completion
     # of all requests, this is the best we can do to gracefully wait while the server responds to requests
-    server.wait_for_termination()
+    await server.wait_for_termination()
