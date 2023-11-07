55addb6ddbc42eb62deebcce607b43d4cbc7f54a
Gerred Dillon <hello@gerred.org>
2023-11-07 09:43:19 -0600

    fix tests

--- leapfrogai/src/leapfrogai/__init__.py
+++ leapfrogai/src/leapfrogai/__init__.py
@@ -1,16 +1,18 @@
 # __init__.py
 
 
-from audio.audio_pb2 import AudioMetadata, AudioRequest, AudioResponse
-from audio.audio_pb2_grpc import Audio, AudioServicer, AudioStub
-from chat.chat_pb2 import (
+from grpc import ServicerContext as GrpcContext
+
+from .audio.audio_pb2 import AudioMetadata, AudioRequest, AudioResponse
+from .audio.audio_pb2_grpc import Audio, AudioServicer, AudioStub
+from .chat.chat_pb2 import (
     ChatCompletionChoice,
     ChatCompletionRequest,
     ChatCompletionResponse,
     ChatItem,
     ChatRole,
 )
-from chat.chat_pb2_grpc import (
+from .chat.chat_pb2_grpc import (
     ChatCompletionService,
     ChatCompletionServiceServicer,
     ChatCompletionServiceStub,
@@ -18,14 +20,14 @@ from chat.chat_pb2_grpc import (
     ChatCompletionStreamServiceServicer,
     ChatCompletionStreamServiceStub,
 )
-from completion.completion_pb2 import (
+from .completion.completion_pb2 import (
     CompletionChoice,
     CompletionFinishReason,
     CompletionRequest,
     CompletionResponse,
     CompletionUsage,
 )
-from completion.completion_pb2_grpc import (
+from .completion.completion_pb2_grpc import (
     CompletionService,
     CompletionServiceServicer,
     CompletionServiceStub,
@@ -33,16 +35,15 @@ from completion.completion_pb2_grpc import (
     CompletionStreamServiceServicer,
     CompletionStreamServiceStub,
 )
-from config import BackendConfig
-from embeddings.embeddings_pb2 import Embedding, EmbeddingRequest, EmbeddingResponse
-from embeddings.embeddings_pb2_grpc import (
+from .config import BackendConfig
+from .embeddings.embeddings_pb2 import Embedding, EmbeddingRequest, EmbeddingResponse
+from .embeddings.embeddings_pb2_grpc import (
     EmbeddingsService,
     EmbeddingsServiceServicer,
     EmbeddingsServiceStub,
 )
-from grpc import ServicerContext as GrpcContext
-from name.name_pb2 import NameResponse
-from name.name_pb2_grpc import NameService, NameServiceServicer, NameServiceStub
-from serve import serve
+from .name.name_pb2 import NameResponse
+from .name.name_pb2_grpc import NameService, NameServiceServicer, NameServiceStub
+from .serve import serve
 
 print("Initializing Leapfrog")
