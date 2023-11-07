4938b256acc992b4b5404522fa95ed01097827c7
Gerred Dillon <hello@gerred.org>
2023-11-07 09:43:19 -0600

    fix tests

--- leapfrogai/tests/test_config.py
+++ leapfrogai/tests/test_config.py
@@ -1,8 +1,18 @@
 import os
 
+os.environ["LEAPFROGAI_CONFIG_FILE"] = "tests/fixtures/config.yaml"
+
+from leapfrogai.chat.chat_pb2 import ChatItem, ChatRole
 from leapfrogai.config import BackendConfig
 
 
 def test_apply_chat_template():
-    os.environ["LEAPFROGAI_CONFIG_FILE"] = "tests/fixtures/config.yaml"
     config = BackendConfig()
+    expected = """<|im_start|>system
+Hello<|im_end|>
+<|im_start|>assistant
+"""
+    assert (
+        config.apply_chat_template([ChatItem(role=ChatRole.SYSTEM, content="Hello")])
+        == expected
+    )
