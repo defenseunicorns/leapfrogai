3042d7dad19a10c7dd950dc9d8de6a1c23eb99bb
Jon Perry <yrrepnoj@gmail.com>
2024-03-21 18:12:58 -0400

    refactor: migrate text-embeddings zarf config to the monorepo

---
+++ packages/text-embeddings/main.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+
+from InstructorEmbedding import INSTRUCTOR
+from leapfrogai_api.types import (
+    Embedding,
+    EmbeddingRequest,
+    EmbeddingResponse,
+    GrpcContext,
+    serve,
+)
+
+model = INSTRUCTOR("./.model")
+
+
+class InstructorEmbedding:
+    async def CreateEmbedding(self, request: EmbeddingRequest, context: GrpcContext):
+        embeddings = model.encode(request.inputs)
+
+        embeddings = [Embedding(embedding=inner_list) for inner_list in embeddings]
+        return EmbeddingResponse(embeddings=embeddings)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(serve(InstructorEmbedding()))
