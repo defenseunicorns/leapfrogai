985b140b21b5fa6d8e668b102ff24a4680428d23
gharvey <gato.harvey@defenseunicorns.com>
2024-04-18 12:28:08 -0700

    Alternative formatting for label

--- packages/supabase/clean-cluster.sh
+++ packages/supabase/clean-cluster.sh
@@ -1,3 +1,4 @@
 helm uninstall supabase -n leapfrogai
 zarf package remove supabase --confirm
 kubectl delete pvc data-supabase-postgresql-0 -n leapfrogai
+kubectl get crd --no-headers | awk '/konghq/ {print $1}' | xargs kubectl delete crd
