ad367e60d92494868448b2e7a454f98ab5986086
Andrew Risse <andrewrisse@gmail.com>
2024-04-22 10:29:53 -0600

    chore: merge main

--- src/leapfrogai_ui/src/lib/components/ChatSidebar.svelte
+++ src/leapfrogai_ui/src/lib/components/ChatSidebar.svelte
@@ -14,7 +14,7 @@
 	import { AddComment } from 'carbon-icons-svelte';
 	import { dates } from '$helpers';
 	import { MAX_LABEL_SIZE } from '$lib/constants';
-	import { conversationsStore, toastStore } from '$stores';
+	import { conversationsStore } from '$stores';
 	import { page } from '$app/stores';
 	import { browser } from '$app/environment';
 	import ImportExport from '$components/ImportExport.svelte';
@@ -33,9 +33,9 @@
 	let scrollOffset = 0;
 	let activeConversationRef: HTMLElement | null;
 	let scrollBoxRef: HTMLElement;
-    let searchText = '';
-    let searchResults: FuseResult<Conversation>[];
-    let filteredConversations: Conversation[] = [];
+	let searchText = '';
+	let searchResults: FuseResult<Conversation>[];
+	let filteredConversations: Conversation[] = [];
 
 	$: activeConversation = $conversationsStore.conversations.find(
 		(conversation) => conversation.id === $page.params.conversation_id
@@ -87,37 +87,32 @@
 		deleteModalOpen = false;
 	};
 
-    const handleActiveConversationChange = (id: string) => {
-        conversationsStore.changeConversation(id);
-        activeConversationRef = document.getElementById(`side-nav-menu-item-${id}`);
-    };
-    const handleActiveConversationChange = (id: string) => {
-        conversationsStore.changeConversation(id);
-        activeConversationRef = document.getElementById(`side-nav-menu-item-${id}`);
-    };
+	const handleActiveConversationChange = (id: string) => {
+		conversationsStore.changeConversation(id);
+		activeConversationRef = document.getElementById(`side-nav-menu-item-${id}`);
+	};
 
-    // To properly display the overflow menu items for each conversation, we have to calculate the height they
-    // should be displayed at due to the carbon override for allowing overflow
-    $: if (browser && activeConversationRef) {
-        menuOffset = activeConversationRef?.offsetTop;
-        scrollOffset = scrollBoxRef?.scrollTop;
-    } else {
-        if (!activeConversationRef) {
-            menuOffset = 0;
-            scrollOffset = 0;
-        }
-    }
+	// To properly display the overflow menu items for each conversation, we have to calculate the height they
+	// should be displayed at due to the carbon override for allowing overflow
+	$: if (browser && activeConversationRef) {
+		menuOffset = activeConversationRef?.offsetTop;
+		scrollOffset = scrollBoxRef?.scrollTop;
+	} else {
+		if (!activeConversationRef) {
+			menuOffset = 0;
+			scrollOffset = 0;
+		}
+	}
 
-    const options = {
-        keys: ['label', 'messages.content']
-    };
+	const options = {
+		keys: ['label', 'messages.content']
+	};
 
-    $: if (searchText) {
-        const fuse = new Fuse($conversationsStore.conversations, options);
-        searchResults = fuse.search(searchText);
-        filteredConversations = searchResults.map((result) => result.item);
-    }
-
+	$: if (searchText) {
+		const fuse = new Fuse($conversationsStore.conversations, options);
+		searchResults = fuse.search(searchText);
+		filteredConversations = searchResults.map((result) => result.item);
+	}
 </script>
 
 <SideNav bind:isOpen={isSideNavOpen} aria-label="side navigation" style="background-color: g90;">
@@ -125,114 +120,117 @@
 		<SideNavItems>
 			<div class="side-nav-items-container">
 				<div style="height: 100%">
-		<SideNavItems>
-			<div class="side-nav-items-container">
-				<div class="new-chat-container">
-					<Button
-						kind="secondary"
-						size="small"
-						icon={AddComment}
-						class="new-chat-btn"
-						id="new-chat-btn"
-						aria-label="new conversation"
-						on:click={() => handleActiveConversationChange('')}>New Chat</Button
-					>
-					<TextInput
-						light
-						size="sm"
-						placeholder="Search..."
-						bind:value={searchText}
-						maxlength={25}
-					/>
-					<SideNavDivider />
-				</div>
+					<div class="side-nav-items-container">
+						<div class="new-chat-container">
+							<Button
+								kind="secondary"
+								size="small"
+								icon={AddComment}
+								class="new-chat-btn"
+								id="new-chat-btn"
+								aria-label="new conversation"
+								on:click={() => handleActiveConversationChange('')}>New Chat</Button
+							>
+							<TextInput
+								light
+								size="sm"
+								placeholder="Search..."
+								bind:value={searchText}
+								maxlength={25}
+							/>
+							<SideNavDivider />
+						</div>
 
-				<div
-					class:noScroll={disableScroll || editMode}
-					bind:this={scrollBoxRef}
-					class="conversations"
-					data-testid="conversations"
-				>
-					{#each organizedConversations as category}
-			{#if category.conversations.length > 0}
-						<SideNavMenu text={category.label} expanded data-testid="side-nav-menu">
-
-								{#each category.conversations as conversation (conversation.id)}
-									<SideNavMenuItem
-										data-testid="side-nav-menu-item-{conversation.label}"
-										id="side-nav-menu-item-{conversation.id}"
-										isSelected={activeConversation?.id === conversation.id}
-										on:click={() => handleActiveConversationChange(conversation.id)}
-									>
-										<div class="menu-content">
-											{#if editMode && activeConversation?.id === conversation.id}
-												<TextInput
-													bind:value={editLabelText}
-													size="sm"
-													class="edit-conversation"
-													on:keydown={(e) => handleEdit(e)}
-													on:blur={(e) => {
-														handleEdit(e);
-													}}
-													autofocus
-													maxlength={MAX_LABEL_SIZE}
-													readonly={editLabelInputDisabled}
-													aria-label="edit conversation"
-												/>
-											{:else}
-												<div data-testid="conversation-label-{conversation.id}" class="menu-text">
-													{conversation.label}
-												</div>
-												<div>
-													<OverflowMenu
-														id={`overflow-menu-${conversation.id}`}
-														on:close={() => {
-															overflowMenuOpen = false;
-															disableScroll = false;
-														}}
-														on:click={(e) => {
-															e.stopPropagation();
-															overflowMenuOpen = true;
-															handleActiveConversationChange(conversation.id);
-															disableScroll = true;
-														}}
-														data-testid="overflow-menu-{conversation.label}"
-														style={overflowMenuOpen && activeConversation?.id === conversation.id
-															? `position: fixed; top: 0; left: 0; transform: translate(224px, ${menuOffset - scrollOffset + 48}px)`
-															: ''}
-													>
-														<OverflowMenuItem
-															text="Edit"
-															on:click={() => {
-																editConversationId = conversation.id;
-																editLabelText = conversation.label;
+						<div
+							class:noScroll={disableScroll || editMode}
+							bind:this={scrollBoxRef}
+							class="conversations"
+							data-testid="conversations"
+						>
+							{#each organizedConversations as category}
+								{#if category.conversations.length > 0}
+									<SideNavMenu text={category.label} expanded data-testid="side-nav-menu">
+										{#each category.conversations as conversation (conversation.id)}
+											<SideNavMenuItem
+												data-testid="side-nav-menu-item-{conversation.label}"
+												id="side-nav-menu-item-{conversation.id}"
+												isSelected={activeConversation?.id === conversation.id}
+												on:click={() => handleActiveConversationChange(conversation.id)}
+											>
+												<div class="menu-content">
+													{#if editMode && activeConversation?.id === conversation.id}
+														<TextInput
+															bind:value={editLabelText}
+															size="sm"
+															class="edit-conversation"
+															on:keydown={(e) => handleEdit(e)}
+															on:blur={(e) => {
+																handleEdit(e);
 															}}
+															autofocus
+															maxlength={MAX_LABEL_SIZE}
+															readonly={editLabelInputDisabled}
+															aria-label="edit conversation"
 														/>
+													{:else}
+														<div
+															data-testid="conversation-label-{conversation.id}"
+															class="menu-text"
+														>
+															{conversation.label}
+														</div>
+														<div>
+															<OverflowMenu
+																id={`overflow-menu-${conversation.id}`}
+																on:close={() => {
+																	overflowMenuOpen = false;
+																	disableScroll = false;
+																}}
+																on:click={(e) => {
+																	e.stopPropagation();
+																	overflowMenuOpen = true;
+																	handleActiveConversationChange(conversation.id);
+																	disableScroll = true;
+																}}
+																data-testid="overflow-menu-{conversation.label}"
+																style={overflowMenuOpen &&
+																activeConversation?.id === conversation.id
+																	? `position: fixed; top: 0; left: 0; transform: translate(224px, ${menuOffset - scrollOffset + 48}px)`
+																	: ''}
+															>
+																<OverflowMenuItem
+																	text="Edit"
+																	on:click={() => {
+																		editConversationId = conversation.id;
+																		editLabelText = conversation.label;
+																	}}
+																/>
 
-														<OverflowMenuItem
-															data-testid="overflow-menu-delete-{conversation.label}"
-															text="Delete"
-															on:click={() => {
-																deleteModalOpen = true;
-															}}
-														/>
-													</OverflowMenu>
+																<OverflowMenuItem
+																	data-testid="overflow-menu-delete-{conversation.label}"
+																	text="Delete"
+																	on:click={() => {
+																		deleteModalOpen = true;
+																	}}
+																/>
+															</OverflowMenu>
+														</div>
+													{/if}
 												</div>
-											{/if}
-										</div>
-									</SideNavMenuItem>
-
-								{/each}
-							</SideNavMenu>
-						{/if}
-					{/each}
+											</SideNavMenuItem>
+										{/each}
+									</SideNavMenu>
+								{/if}
+							{/each}
+						</div>
+						<div>
+							<SideNavDivider />
+							<ImportExport />
+						</div>
+					</div>
 				</div>
-				<div>
-					<SideNavDivider />
-					<ImportExport />
-				</div>
-			</div>
-		</SideNavItems>
+			</div></SideNavItems
+		>
 
 		<Modal
 			danger
