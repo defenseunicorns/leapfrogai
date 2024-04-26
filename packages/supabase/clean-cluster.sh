5a4c6bb27356f52fa2087ab4fea67cdf1b6f9056
gharvey <gato.harvey@defenseunicorns.com>
2024-04-26 16:54:07 -0700

    Improves clena script and changes password name

--- packages/supabase/clean-cluster.sh
+++ packages/supabase/clean-cluster.sh
@@ -1,4 +1,6 @@
-helm uninstall supabase -n leapfrogai
 zarf package remove supabase --confirm
+helm uninstall supabase -n leapfrogai
+helm uninstall supabase-bootstrap -n leapfrogai
+helm uninstall supabase-secrets-generator -n leapfrogai
 kubectl delete pvc data-supabase-postgresql-0 -n leapfrogai
 kubectl get crd --no-headers | awk '/konghq/ {print $1}' | xargs kubectl delete crd
