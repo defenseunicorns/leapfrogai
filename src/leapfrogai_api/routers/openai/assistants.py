cba967646b3f0e4cabd833b53c51fefaa30cda78
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-04-29 18:29:42 -0400

    chore: refactoring API and adding stubs for new OpenAI endpoints (#437)
    * moving over assistants stuff
    
    * adding in some files endpoints stuff
    
    * Addressing formatting issues
    
    * add stubs for vector store
    
    * add threads stubs
    
    * stub messages and vector store files
    
    * fix openai typing refs
    
    * stubbing out runs and runs steps
    
    * typing issues, and removing tests
    
    * only include endpoints if Supabase is available
    
    * fix formatting issue
    
    * more robustly handle missing supabase
    
    * refactor supabase_wrapper
    
    * snazzy tests
    
    * refactor Supabase Wrapper
    
    * remove supabase-jwt secret (for now)
    
    * update stubs to async defs
    
    * new branch to focus on refactoring API and adding new stubs for OpenAI endpoints
    
    * revert Zarf.yaml changes
    
    * we don't need .env until we have Supabase
    
    * refactoring tests
    
    * fix formatting issues
    
    * removing Supabase stuff (for now)
    
    * refactoring api
    
    * fix formatting issues pre-commit didn't catch...
    
    * add back legacy completions endpoint (for now)
    
    * some rework
    
    * ensure pytest runs
    
    * adding back the custom Usage class
    
    * fixing blank space
    
    * moving types to backend
    
    * updating typing for helpers
    
    * deprecate legacy Optional typing
    
    * deprecate legacy List typing
    
    * fix nonetype error
    
    * fixing Usage convention

---
+++ src/leapfrogai_api/routers/openai/assistants.py
@@ -0,0 +1,47 @@
+"""OpenAI Compliant Assistants API Router."""
+
+from fastapi import HTTPException, APIRouter
+from openai.types.beta import Assistant, AssistantDeleted
+from leapfrogai_api.backend.types import (
+    CreateAssistantRequest,
+    ModifyAssistantRequest,
+)
+
+router = APIRouter(prefix="/openai/v1/assistants", tags=["openai/assistants"])
+
+
+@router.post("")
+async def create_assistant(request: CreateAssistantRequest) -> Assistant:
+    """Create an assistant."""
+    # TODO: Implement this function
+    raise HTTPException(status_code=501, detail="Not implemented")
+
+
+@router.get("")
+async def list_assistants() -> list[Assistant]:
+    """List all the assistants."""
+    # TODO: Implement this function
+    raise HTTPException(status_code=501, detail="Not implemented")
+
+
+@router.get("/{assistant_id}")
+async def retrieve_assistant(assistant_id: str) -> Assistant:
+    """Retrieve an assistant."""
+    # TODO: Implement this function
+    raise HTTPException(status_code=501, detail="Not implemented")
+
+
+@router.post("/{assistant_id}")
+async def modify_assistant(
+    assistant_id: str, request: ModifyAssistantRequest
+) -> Assistant:
+    """Modify an assistant."""
+    # TODO: Implement this function
+    raise HTTPException(status_code=501, detail="Not implemented")
+
+
+@router.delete("/{assistant_id}")
+async def delete_assistant(assistant_id: str) -> AssistantDeleted:
+    """Delete an assistant."""
+    # TODO: Implement this function
+    raise HTTPException(status_code=501, detail="Not implemented")
