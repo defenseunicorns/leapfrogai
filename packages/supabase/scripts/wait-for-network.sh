b8a30b8d68b8c9a2bd62608a9633373530917d1b
gharvey <gato.harvey@defenseunicorns.com>
2024-05-01 16:33:10 -0700

    Look for ready sand status to confirm deployment has been rolled out

--- packages/supabase/scripts/wait-for-network.sh
+++ packages/supabase/scripts/wait-for-network.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
-
 DEPLOYMENT_NAME="kong"
 NAMESPACE="leapfrogai"
 DESIRED_PHASE="Running"
+DESIRED_READY_STATUS="True"
 
+# Function to get the pod status and ready status
 get_pod_status() {
-    POD_STATUSES=$(kubectl get pods -l app.kubernetes.io/name=$DEPLOYMENT_NAME -n $NAMESPACE -o jsonpath='{range .items[*]}{.status.phase}{" "}{end}')
+    POD_STATUSES=$(kubectl get pods -l app.kubernetes.io/name=$DEPLOYMENT_NAME -n $NAMESPACE -o jsonpath='{range .items[*]}{.status.phase}{" "}{.status.containerStatuses[*].ready}{" "}{end}')
 }
 
+# Function to check the deployment status
 check_deployment_status() {
     get_pod_status
-    # Check if all pods are in the desired phase
-    if [[ "$POD_STATUSES" == *"$DESIRED_PHASE"* ]] && ! [[ "$POD_STATUSES" == *"Pending"* ]]; then
-        echo "All pods of deployment $DEPLOYMENT_NAME are in the $DESIRED_PHASE phase."
+    # Check if all pods are in the desired phase and ready status
+    if [[ "$POD_STATUSES" == *"$DESIRED_PHASE"* ]] && [[ "$POD_STATUSES" == *"$DESIRED_READY_STATUS"* ]] && ! [[ "$POD_STATUSES" == *"Pending"* ]]; then
+        echo "All pods of deployment $DEPLOYMENT_NAME are in the $DESIRED_PHASE phase and ready."
         return 0 # Return success
     else
-        echo "Not all pods of deployment $DEPLOYMENT_NAME are in the $DESIRED_PHASE phase. Current status: $POD_STATUSES"
+        echo "Not all pods of deployment $DEPLOYMENT_NAME are in the $DESIRED_PHASE phase and ready. Current status: $POD_STATUSES"
         return 1 # Return failure
     fi
 }
 
-# Keep checking the deployment status until all pods are in the desired phase
+# Keep checking the deployment status until all pods are in the desired phase and ready
 while true; do
     check_deployment_status
     if [ $? -eq 0 ]; then
-        break # Exit the loop if all pods are in the desired phase
+        break # Exit the loop if all pods are in the desired phase and ready
     fi
     sleep 5 # Wait for 5 seconds before checking again
 done
