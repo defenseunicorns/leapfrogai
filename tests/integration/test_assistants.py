31de889ae291b705ce72bd07c774e3c6ee73f959
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-05-02 14:25:13 -0400

    improving integration tests

--- tests/integration/test_assistants.py
+++ tests/integration/test_assistants.py
@@ -1,7 +1,7 @@
 """Test the API endpoints for assistants."""
 
 from fastapi.testclient import TestClient
-from openai.types.beta import Assistant
+from openai.types.beta import Assistant, AssistantDeleted
 
 from leapfrogai_api.routers.openai.assistants import router
 from leapfrogai_api.backend.types import (
@@ -33,10 +33,14 @@ def test_assistants():
 
     list_response = client.get("/openai/v1/assistants")
     assert list_response.status_code == 200
-    assert Assistant.model_validate(list_response.json()[0])
+    for assistant_object in list_response.json():
+        assert Assistant.model_validate(
+            assistant_object
+        ), "Should return a list of Assistants."
 
     get_response = client.get(f"/openai/v1/assistants/{create_response.json()['id']}")
     assert get_response.status_code == 200
+    assert Assistant.model_validate(get_response.json()), "Should return an Assistant."
 
     request = ModifyAssistantRequest(
         model="test1",
@@ -56,17 +60,41 @@ def test_assistants():
         json=request.model_dump(),
     )
     assert modify_response.status_code == 200
-    assert Assistant.model_validate(modify_response.json())
+    assert Assistant.model_validate(
+        modify_response.json()
+    ), "Should return a Assistant."
+    assert modify_response.json()["model"] == "test1", "Should be modified."
 
     get_modified_response = client.get(
         f"/openai/v1/assistants/{create_response.json()['id']}"
     )
     assert get_modified_response.status_code == 200
+    assert Assistant.model_validate(
+        get_modified_response.json()
+    ), "Should return a Assistant."
+    assert get_modified_response.json()["model"] == "test1", "Should be modified."
 
     delete_response = client.delete(
         f"/openai/v1/assistants/{create_response.json()['id']}"
     )
     assert delete_response.status_code == 200
+    assert AssistantDeleted.model_validate(
+        delete_response.json()
+    ), "Should return a AssistantDeleted object."
+    assert delete_response.json()["deleted"] is True, "Should be able to delete."
+
+    delete_response = client.delete(
+        f"/openai/v1/assistants/{create_response.json()['id']}"
+    )
+    assert (
+        delete_response.status_code == 200
+    ), "Should return 200 even if the assistant is not found."
+    assert AssistantDeleted.model_validate(
+        delete_response.json()
+    ), "Should return a AssistantDeleted object."
+    assert (
+        delete_response.json()["deleted"] is False
+    ), "Should not be able to delete twice."
 
     # Make sure the assistant is not still present
     retrieve_assistant_response = client.get(
--- tests/integration/test_files.py
+++ tests/integration/test_files.py
@@ -23,20 +23,40 @@ def test_files():
 
     list_response = client.get("/openai/v1/files")
     assert list_response.status_code == 200
-    assert FileObject.model_validate(list_response.json()[0])
+    for file_object in list_response.json():
+        assert FileObject.model_validate(
+            file_object
+        ), "Should return a list of FileObjects."
 
     get_response = client.get(f"/openai/v1/files/{create_response.json()['id']}")
     assert get_response.status_code == 200
+    assert FileObject.model_validate(get_response.json()), "Should return a FileObject."
 
     get_content_response = client.get(
         f"/openai/v1/files/{create_response.json()['id']}/content"
     )
     assert get_content_response.status_code == 200
-    assert testfile_content.decode() in get_content_response.text
+    assert (
+        testfile_content.decode() in get_content_response.text
+    ), "Should return the file content."
 
     delete_response = client.delete(f"/openai/v1/files/{create_response.json()['id']}")
     assert delete_response.status_code == 200
-    assert FileDeleted.model_validate(delete_response.json())
+    assert FileDeleted.model_validate(
+        delete_response.json()
+    ), "Should return a FileDeleted object."
+    assert delete_response.json()["deleted"] is True, "Should be able to delete."
+
+    delete_response = client.delete(f"/openai/v1/files/{create_response.json()['id']}")
+    assert (
+        delete_response.status_code == 200
+    ), "Should return 200 even if the file is not found."
+    assert FileDeleted.model_validate(
+        delete_response.json()
+    ), "Should return a FileDeleted object."
+    assert (
+        delete_response.json()["deleted"] is False
+    ), "Should not be able to delete twice."
 
     # Make sure the assistant is not still present
     get_response = client.get(f"/openai/v1/files/{create_response.json()['id']}")
