cbe8beed23865f09efc055bbd874434e818668dc
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-05-02 17:22:48 -0400

    working on vector stores routes

--- src/leapfrogai_api/routers/openai/vector_store.py
+++ src/leapfrogai_api/routers/openai/vector_store.py
@@ -10,18 +10,18 @@ router = APIRouter(prefix="/openai/v1/vector_store", tags=["openai/vector_store"
 
 
 @router.post("")
-async def create_vector_store() -> VectorStore:
+async def create_vector_store(session: Session) -> VectorStore:
     """Create a vector store."""
     # TODO: Implement this function
     raise HTTPException(status_code=501, detail="Not implemented")
 
 
 @router.get("")
-async def list_vector_stores(client: Session) -> list[VectorStore] | None:
+async def list_vector_stores(session: Session) -> list[VectorStore] | None:
     """List all the vector stores."""
     try:
         crud_vector_store = CRUDVectorStore(model=VectorStore)
-        return await crud_vector_store.list(client=client)
+        return await crud_vector_store.list(client=session)
     except Exception as exc:
         raise HTTPException(
             status_code=500, detail="Failed to list vector stores"
@@ -45,28 +45,41 @@ async def retrieve_vector_store(
 
 
 @router.post("/{vector_store_id}")
-async def modify_vector_store(vector_store_id: str) -> VectorStore:
+async def modify_vector_store(session: Session, vector_store_id: str) -> VectorStore:
     """Modify a vector store."""
     # TODO: Implement this function
     raise HTTPException(status_code=501, detail="Not implemented")
 
 
 @router.delete("/{vector_store_id}")
-async def delete_vector_store(vector_store_id: str) -> VectorStoreDeleted:
+async def delete_vector_store(
+    session: Session, vector_store_id: str
+) -> VectorStoreDeleted:
     """Delete a vector store."""
-    # TODO: Implement this function
-    raise HTTPException(status_code=501, detail="Not implemented")
+    try:
+        crud_vector_store = CRUDVectorStore(model=VectorStore)
+        return await crud_vector_store.delete(
+            client=session, vector_store_id=vector_store_id
+        )
+    except Exception as exc:
+        raise HTTPException(
+            status_code=500, detail="Failed to delete vector store"
+        ) from exc
 
 
 @router.post("/{vector_store_id}/files")
-async def create_vector_store_file(vector_store_id: str) -> VectorStoreFile:
+async def create_vector_store_file(
+    session: Session, vector_store_id: str
+) -> VectorStoreFile:
     """Create a file in a vector store."""
     # TODO: Implement this function
     raise HTTPException(status_code=501, detail="Not implemented")
 
 
 @router.get("/{vector_store_id}/files")
-async def list_vector_store_files(vector_store_id: str) -> list[VectorStoreFile]:
+async def list_vector_store_files(
+    session: Session, vector_store_id: str
+) -> list[VectorStoreFile]:
     """List all the files in a vector store."""
     # TODO: Implement this function
     raise HTTPException(status_code=501, detail="Not implemented")
@@ -74,7 +87,7 @@ async def list_vector_store_files(vector_store_id: str) -> list[VectorStoreFile]
 
 @router.delete("/{vector_store_id}/files/{file_id}")
 async def delete_vector_store_file(
-    vector_store_id: str, file_id: str
+    session: Session, vector_store_id: str, file_id: str
 ) -> VectorStoreFileDeleted:
     """Delete a file in a vector store."""
     # TODO: Implement this function
