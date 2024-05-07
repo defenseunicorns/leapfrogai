30cc128c5dd0d5d44308e1d3207bab6b72f138d0
gharvey <gato.harvey@defenseunicorns.com>
2024-05-07 12:09:11 -0700

    Ruff linting and keycloak secret refactor

--- tests/e2e/test_supabase.py
+++ tests/e2e/test_supabase.py
@@ -4,7 +4,7 @@ import os
 health_urls = {
     "auth_health_url": "http://localhost:8000/auth/v1/health",
     "rest_health_url": "http://localhost:8000/rest/v1/",
-    "storage_health_url": "http://localhost:8000/storage/v1/status"
+    "storage_health_url": "http://localhost:8000/storage/v1/status",
 }
 anon_api_key = os.environ["API_KEY"]
 
@@ -12,8 +12,9 @@ anon_api_key = os.environ["API_KEY"]
 def test_studio():
     try:
         for url_name in health_urls:
-            response = requests.get(health_urls[url_name], headers={
-                                    "apikey": anon_api_key})
+            response = requests.get(
+                health_urls[url_name], headers={"apikey": anon_api_key}
+            )
             response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"Error: Request failed with status code {response.status_code}")
