487562f9f44b090d99942855a4632ba76e45defd
Andrew Risse <andrewrisse@gmail.com>
2024-05-13 11:28:52 -0600

    fix: Formatting for migrations file

--- src/leapfrogai_ui/supabase/migrations/20240513155910_all_migrations.sql
+++ src/leapfrogai_ui/supabase/migrations/20240513155910_all_migrations.sql
@@ -1,39 +1,66 @@
 -- Create tables
 create table conversations (
-                               id uuid primary key DEFAULT uuid_generate_v4(),
-                               user_id uuid references auth.users not null,
-                               label text,
-                               inserted_at timestamp with time zone default timezone('utc'::text, now()) not null
+   id uuid primary key DEFAULT uuid_generate_v4(),
+   user_id uuid references auth.users not null,
+   label text,
+   inserted_at timestamp with time zone default timezone('utc'::text, now()) not null
 );
 
 create table messages (
-                          id uuid primary key DEFAULT uuid_generate_v4(),
-                          user_id uuid references auth.users not null,
-                          conversation_id uuid references conversations on delete cascade not null,
-                          role text check (role in ('system', 'user', 'assistant', 'function', 'data', 'tool')),
-                          content text,
-                          inserted_at timestamp with time zone default timezone('utc'::text, now()) not null
+  id uuid primary key DEFAULT uuid_generate_v4(),
+  user_id uuid references auth.users not null,
+  conversation_id uuid references conversations on delete cascade not null,
+  role text check (role in ('system', 'user', 'assistant', 'function', 'data', 'tool')),
+  content text,
+  inserted_at timestamp with time zone default timezone('utc'::text, now()) not null
 );
 
--- Create a table for public profiles
+
 create table profiles (
-                          id uuid references auth.users not null primary key,
-                          updated_at timestamp with time zone,
-                          username text unique,
-                          full_name text,
-                          avatar_url text,
-                          website text,
+  id uuid references auth.users not null primary key,
+  updated_at timestamp with time zone,
+  username text unique,
+  full_name text,
+  avatar_url text,
+  website text,
 
-                          constraint username_length check (char_length(username) >= 3)
+  constraint username_length check (char_length(username) >= 3)
 );
 
--- conversations, messages, and profiles tables and RLS policies
+create table assistants (
+    id uuid primary key DEFAULT uuid_generate_v4(),
+    object text check (object in ('assistant')),
+    name varchar(255),
+    description varchar(512),
+    model varchar(255) not null,
+    instructions TEXT,
+    tools jsonb,
+    tool_resources jsonb,
+    metadata jsonb,
+    temperature float,
+    top_p float,
+    response_format jsonb,
+created_at timestamp with time zone default timezone('utc'::text, now()) not null
+);
+
+
+-- Set up Storage!
+insert into storage.buckets
+(id, name, public)
+values
+    ('assistant_avatars', 'assistant_avatars', true);
+
+-- These are user profiles avatars, currently not used by app and will be removed soon
+insert into storage.buckets (id, name)
+values ('avatars', 'avatars');
+
+
+-- RLS policies
 
 alter table conversations enable row level security;
-
 alter table messages enable row level security;
-
 alter table profiles enable row level security;
+alter table assistants enable row level security;
 
 -- Policies for conversations
 create policy "Individuals can create conversations." on conversations for
@@ -58,21 +85,27 @@ create policy "Individuals can delete their own messages." on messages for
 -- Policies for profiles
 create policy "Public profiles are viewable by everyone." on profiles
     for select using (true);
-
 create policy "Users can insert their own profile." on profiles
   for insert with check (auth.uid() = id);
-
 create policy "Users can update own profile." on profiles
   for update using (auth.uid() = id);
 
--- Set up access controls for storage.
--- See https://supabase.com/docs/guides/storage/security/access-control#policy-examples for more details.
+-- Policies for assistants
+create policy "Individuals can view their own assistants." ON assistants
+for select using ((metadata ->> 'created_by') = auth.uid()::text);
+create policy "Individuals can create assistants." on assistants for
+    insert with check ((metadata ->> 'created_by') = auth.uid()::text);
+create policy "Individuals can update their own assistants." on assistants for
+update using ((metadata ->> 'created_by') = auth.uid()::text);
+create policy "Individuals can delete their own assistants." on assistants for
+    delete using ((metadata ->> 'created_by') = auth.uid()::text);
+
+
+-- Policies for storage.
 create policy "Avatar images are publicly accessible." on storage.objects
   for select using (bucket_id = 'avatars');
-
 create policy "Anyone can upload an avatar." on storage.objects
   for insert with check (bucket_id = 'avatars');
-
 create policy "Anyone can update their own avatar." on storage.objects
   for update using (auth.uid() = owner) with check (bucket_id = 'avatars');
 
@@ -91,43 +124,4 @@ create trigger on_auth_user_created
     after insert on auth.users
     for each row execute procedure public.handle_new_user();
 
-
-CREATE TABLE assistants (
-                            id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-                            object text CHECK (object in ('assistant')),
-                            name VARCHAR(255),
-                            description VARCHAR(512),
-                            model VARCHAR(255) NOT NULL,
-                            instructions TEXT,
-                            tools jsonb,
-                            tool_resources jsonb,
-                            metadata jsonb,
-                            temperature FLOAT,
-                            top_p FLOAT,
-                            response_format jsonb,
-                            created_at timestamp with time zone default timezone('utc'::text, now()) not null
-);
-
-alter table assistants enable row level security;
-
--- Policies for assistants
-CREATE POLICY "Individuals can view their own assistants." ON assistants
-FOR SELECT USING ((metadata ->> 'created_by') = auth.uid()::text);
-create policy "Individuals can create assistants." on assistants for
-    insert with check ((metadata ->> 'created_by') = auth.uid()::text);
-create policy "Individuals can update their own assistants." on assistants for
-update using ((metadata ->> 'created_by') = auth.uid()::text);
-create policy "Individuals can delete their own assistants." on assistants for
-    delete using ((metadata ->> 'created_by') = auth.uid()::text);
-
-
--- Set up Storage!
-insert into storage.buckets
-(id, name, public)
-values
-    ('assistant_avatars', 'assistant_avatars', true);
-
--- These are user profiles avatars, currently not used by app
-insert into storage.buckets (id, name)
-values ('avatars', 'avatars');
 
