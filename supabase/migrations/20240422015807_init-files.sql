1f5c6510fbda2b8cc7c4c75b2ca941320875c85b
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-05-16 12:29:01 -0400

    a couple schema tweaks

--- supabase/migrations/20240422015807_init-files.sql
+++ supabase/migrations/20240422015807_init-files.sql
@@ -3,9 +3,9 @@ create table
   file_objects (
     id uuid primary key DEFAULT uuid_generate_v4(),
     bytes int,
-    created_at bigint default extract(epoch from now()),
+    created_at bigint default extract(epoch from now()) not null,
     filename text,
-    object text,
+    object text check (object in ('file')),
     purpose text,
     status text,
     status_details text
