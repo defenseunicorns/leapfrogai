ea8436b0f128f7224ca642e3d5912ca87814986e
gharvey <gato.harvey@defenseunicorns.com>
2024-05-21 19:16:05 -0700

    Adds select RLS for file retrieval

--- supabase/migrations/20240516152530_enable_rls.sql
+++ supabase/migrations/20240516152530_enable_rls.sql
@@ -23,8 +23,9 @@ create policy "Individuals can delete their own file_objects." on file_objects f
     delete using (auth.uid() = user_id);
 
 -- Policies for file_bucket
-create policy "Authenticated individual can add files to file_bucket"
-on storage.objects for insert to authenticated with check (
-    -- restrict bucket
-    bucket_id = 'file_bucket'
-);
+create policy "Authenticated individuals can add files to file_bucket."
+on storage.objects for
+    insert to authenticated with check (bucket_id = 'file_bucket');
+create policy "Authenticated individuals can view files in the file_bucket."
+on storage.objects for
+    select to authenticated using (bucket_id = 'file_bucket');
