4bc479d4566fca451e8622f428aeaca55c864040
Andrew Risse <andrewrisse@gmail.com>
2024-05-24 10:09:32 -0600

    chore: update migration

--- packages/supabase/migrations/20240523233951_0.8_ui_migration.sql
+++ packages/supabase/migrations/20240523233951_0.8_ui_migration.sql
@@ -2,16 +2,9 @@ DO $$
 BEGIN
     BEGIN
         DROP TABLE IF EXISTS messages;
-    EXCEPTION
-        WHEN others THEN
-            -- do nothing, fail silently
     END;
-
     BEGIN
         DROP TABLE IF EXISTS conversations;
-    EXCEPTION
-        WHEN others THEN
-            -- do nothing, fail silently
     END;
 END $$;
 
