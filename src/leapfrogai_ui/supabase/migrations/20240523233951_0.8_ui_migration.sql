9aabb7e0eb159bdcd03ac4166c1df01c34056a14
Andrew Risse <andrewrisse@gmail.com>
2024-05-24 09:58:31 -0600

    fix: remove unnecessary exception handling

--- src/leapfrogai_ui/supabase/migrations/20240523233951_0.8_ui_migration.sql
+++ src/leapfrogai_ui/supabase/migrations/20240523233951_0.8_ui_migration.sql
@@ -2,16 +2,10 @@ DO $$
 BEGIN
     BEGIN
         DROP TABLE IF EXISTS messages;
-    EXCEPTION
-        WHEN others THEN
-            -- do nothing, fail silently
     END;
 
     BEGIN
         DROP TABLE IF EXISTS conversations;
-    EXCEPTION
-        WHEN others THEN
-            -- do nothing, fail silently
     END;
 END $$;
 
