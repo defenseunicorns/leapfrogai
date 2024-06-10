8d13d9b6253cba47428aebdb2f2c7c2ef28cd80f
Jon Perry <yrrepnoj@gmail.com>
2024-06-10 14:50:56 -0400

    chore: re-add the migrations that were present during the 0.7.2 release

--- packages/api/supabase/migrations/20240322174520_v0.7.2_api_sql_schema.sql
+++ packages/api/supabase/migrations/20240322174520_v0.7.2_api_sql_schema.sql
@@ -3,9 +3,11 @@ create table
   file_objects (
     id uuid primary key DEFAULT uuid_generate_v4(),
     bytes int,
-    created_at bigint default extract(epoch from now()) not null,
+    created_at bigint,
+    -- created_at bigint default extract(epoch from now()) not null, -- This line was added post v0.7.2
     filename text,
-    object text check (object in ('file')),
+    object text,
+    -- object text check (object in ('file')), -- This line was added post v0.7.2
     purpose text,
     status text,
     status_details text
