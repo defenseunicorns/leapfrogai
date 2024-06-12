7bb1a93c6e11553d772747426498c5c53f9fe284
Jon Perry <yrrepnoj@gmail.com>
2024-06-12 17:03:53 -0400

    chore: update the handle_new_user sql function

--- src/leapfrogai_ui/supabase/migrations/20240322174521_v0.7.2_ui_sql_schema.sql
+++ src/leapfrogai_ui/supabase/migrations/20240322174521_v0.7.2_ui_sql_schema.sql
@@ -111,8 +111,8 @@ DROP FUNCTION IF EXISTS public.handle_new_user();
 create function public.handle_new_user()
     returns trigger as $$
 begin
-insert into public.profiles (id, full_name, avatar_url)
-values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url');
+insert into public.profiles (id, full_name)
+values (new.id, new.raw_user_meta_data->>'full_name');
 return new;
 end;
 $$ language plpgsql security definer;
