79399a38cd81b8ec05e918e7789e0939c147d14b
alekst23 <alekst23@gmail.com>
2024-06-13 16:12:05 -0400

    Testing environment setup via conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+def pytest_configure(config):
+    # runs once for all tests
+
+    load_dotenv()
+
+    os.environ["SUPABASE_USER_JWT"] = os.environ.get("SUPABASE_USER_JWT", "mock-data")
+    os.environ["SUPABASE_URL"] = os.environ.get("SUPABASE_URL", "mock-data")
+    os.environ["ANON_KEY"] = os.environ.get("SUPABASE_ANON_KEY", "mock-data")
+    os.environ["API_KEY"] = os.environ.get("SUPABASE_ANON_KEY", "mock-data")
+    
+
