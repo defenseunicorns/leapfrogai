9727594b42f41ecd0e2366f29611a3e9b61a29f9
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-06-16 18:52:19 -0400

    removing unused run steps schema

--- packages/api/supabase/migrations/20240611111500_runs.sql
+++ packages/api/supabase/migrations/20240611111500_runs.sql
@@ -33,31 +33,8 @@ create table
     response_format jsonb
   );
 
--- Create a table to store the OpenAI Run Step Objects
-create table
-  run_step_objects (
-    id uuid primary key DEFAULT uuid_generate_v4(),
-    user_id uuid references auth.users not null,
-    object text check (object in ('thread.run.step')),
-    created_at bigint default extract(epoch FROM NOW()) not null,
-    assistant_id uuid references assistant_objects (id) on delete set null,
-    thread_id uuid references thread_objects (id) on delete cascade not null,
-    run_id uuid references run_objects (id) on delete cascade not null,
-    step_type text,
-    status text,
-    step_details jsonb,
-    last_error jsonb,
-    expired_at bigint,
-    cancelled_at bigint,
-    failed_at bigint,
-    completed_at bigint,
-    metadata jsonb,
-    usage jsonb
-  );
-
 -- RLS policies
 alter table run_objects enable row level security;
-alter table run_step_objects enable row level security;
 
 -- Policies for run_objects
 create policy "Individuals can view their own run_objects." on run_objects for
@@ -68,21 +45,6 @@ create policy "Individuals can update their own run_objects." on run_objects for
     update using (auth.uid() = user_id);
 create policy "Individuals can delete their own run_objects." on run_objects for
     delete using (auth.uid() = user_id);
-
--- Policies for run_step_objects
-create policy "Individuals can view their own run_step_objects." on run_step_objects for
-    select using (auth.uid() = user_id);
-create policy "Individuals can create run_step_objects." on run_step_objects for
-    insert with check (auth.uid() = user_id);
-create policy "Individuals can update their own run_step_objects." on run_step_objects for
-    update using (auth.uid() = user_id);
-create policy "Individuals can delete their own run_step_objects." on run_step_objects for
-    delete using (auth.uid() = user_id);
-
--- Indexes for common filtering and sorting for run_step_objects
-CREATE INDEX run_step_id ON run_step_objects (id);
-CREATE INDEX run_step_user_id ON run_step_objects (user_id);
-CREATE INDEX run_step_created_at ON run_step_objects (created_at);
 
 -- Indexes for common filtering and sorting for run_objects
 CREATE INDEX run_objects_id ON run_objects (id);
