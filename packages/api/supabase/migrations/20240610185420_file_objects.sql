37badaeeee7d41e8dace24b94c0e52ac96594e59
Jonathan Perry <YrrepNoj@gmail.com>
2024-06-18 13:21:41 -0400

    chore: Handle migrations with least permissions necessary (#630)
    * chore: use standard 'postgres' user for API and UI migrations
    * chore: consolidate API and UI migration Dockerfiles into a single source
    * chore: add migration component to supabase pacakge
        * The supabase package will be responsible for running migrations that require elevated permissions

--- packages/api/supabase/migrations/20240610185420_file_objects.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE file_objects ALTER COLUMN created_at SET DEFAULT extract(epoch from now());
-ALTER TABLE file_objects ALTER COLUMN created_at SET NOT NULL;
-ALTER TABLE file_objects ADD CHECK (object in ('file'));
