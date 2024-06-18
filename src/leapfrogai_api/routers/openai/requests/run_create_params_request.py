48014e78a1deea47f264d7f776a9912a0709a3fd
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-06-18 00:29:45 -0400

    adding type hints

--- src/leapfrogai_api/routers/openai/requests/run_create_params_request.py
+++ src/leapfrogai_api/routers/openai/requests/run_create_params_request.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import AsyncGenerator, Any
-from openai.types.beta.threads import MessageContent, Run
+from openai.types.beta.threads import Run
 from openai.types.beta.threads.run_create_params import (
     AdditionalMessage,
     AdditionalMessageAttachment,
@@ -24,7 +24,7 @@ from leapfrogai_api.routers.supabase_session import Session
 class RunCreateParamsRequestBaseRequest(RunCreateParamsRequestBase):
     additional_instructions: str | None = Field(default=None, examples=[""])
     additional_messages: list[AdditionalMessage] | None = Field(
-        default=None,
+        default=[],
         examples=[
             [
                 AdditionalMessage(
@@ -47,35 +47,38 @@ class RunCreateParamsRequestBaseRequest(RunCreateParamsRequestBase):
     )
     stream: bool | None = Field(default=None, examples=[False])
 
-    async def create_additional_messages(self, session, thread_id):
+    async def create_additional_messages(self, session: Session, thread_id: str):
         """If additional messages exist, create them in the DB as a part of this thread"""
         if self.additional_messages:
             for additional_message in self.additional_messages:
                 # Convert the messages content into the correct format
-                message_content: MessageContent = from_content_param_to_content(
-                    additional_message.get("content")
-                )
+                if content := additional_message.get("content"):
+                    message_content = from_content_param_to_content(
+                        thread_message_content=content
+                    )
 
                 create_message_request = CreateMessageRequest(
-                    role=additional_message.get("role"),
+                    role=additional_message["role"],
                     content=[message_content],
                     attachments=additional_message.get("attachments"),
                     metadata=additional_message.get("metadata"),
                 )
 
-                await create_message_request.create_message(session, thread_id)
+                await create_message_request.create_message(
+                    session=session, thread_id=thread_id
+                )
 
     async def update_with_assistant_data(self, session: Session):
-        await super().update_with_assistant_data(session)
+        await super().update_with_assistant_data(session=session)
 
     async def create_run(self, session, thread_id):
-        await self.update_with_assistant_data(session)
+        """Create a run."""
+        await self.update_with_assistant_data(session=session)
 
-        await self.create_additional_messages(session, thread_id)
+        await self.create_additional_messages(session=session, thread_id=thread_id)
 
-        create_params: RunCreateParamsRequestBase = RunCreateParamsRequestBase(
-            **self.__dict__
-        )
+        create_params = RunCreateParamsRequestBase.model_validate(self.__dict__)
+
         crud_run = CRUDRun(db=session)
         run = Run(
             id="",  # Leave blank to have Postgres generate a UUID
@@ -91,13 +94,13 @@ class RunCreateParamsRequestBaseRequest(RunCreateParamsRequestBase):
         """Generate a new response based on the existing thread"""
         if self.stream:
             initial_messages: list[str] = (
-                RunCreateParamsRequestBase.get_initial_messages_base(new_run)
+                RunCreateParamsRequestBase.get_initial_messages_base(run=new_run)
             )
             ending_messages: list[str] = (
-                RunCreateParamsRequestBase.get_ending_messages_base(new_run)
+                RunCreateParamsRequestBase.get_ending_messages_base(run=new_run)
             )
             stream: AsyncGenerator[str, Any] = (
-                super().stream_generate_message_for_thread(session, initial_messages, existing_thread, ending_messages, new_run.id, self.additional_instructions)
+                super().stream_generate_message_for_thread(session=session, initial_messages=initial_messages, thread=existing_thread, ending_messages=ending_messages, run_id=new_run.id, additional_instructions=self.additional_instructions)
             )
 
             return StreamingResponse(stream, media_type="text/event-stream")
