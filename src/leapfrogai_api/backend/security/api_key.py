2014957f32f62017c39c59e2d7d1f8d0d2ea092c
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-06-20 14:37:20 -0400

    broke up routes and basic api key operations

---
+++ src/leapfrogai_api/backend/security/api_key.py
@@ -0,0 +1,64 @@
+"""Basic API key security functions."""
+
+import secrets
+import hashlib
+
+KEY_PREFIX = "lfai"
+
+
+def generate_api_key() -> tuple[str, str]:
+    """Generate an API key.
+
+    returns:
+        read_once_token: str
+        hashed_token: str
+    """
+    unique_key = secrets.token_bytes(32).hex()
+    hashed_token = encode_unique_key(unique_key)
+    checksum = parse(hashed_token)[2]
+
+    read_once_token = f"{KEY_PREFIX}_{unique_key}_{checksum}"
+    hashed_token = encode_unique_key(unique_key)
+
+    return read_once_token, hashed_token
+
+
+def encode_unique_key(unique_key: str):
+    """Hashes and encodes an API key as a string.
+
+    returns:
+        api_key: str # in the format {prefix}_{one_way_hash}_{checksum}
+    """
+    one_way_hash = hashlib.sha256(unique_key.encode()).hexdigest()
+    checksum = hashlib.sha256(unique_key.encode()).hexdigest()[:4]
+    return f"{KEY_PREFIX}_{one_way_hash}_{checksum}"
+
+
+def validate_checksum(unique_key: str, checksum: str):
+    """Validate the checksum of an API key."""
+    return hashlib.sha256(unique_key.encode()).hexdigest()[:4] == checksum
+
+
+def parse(api_key: str) -> tuple[str, str, str]:
+    """Parse an API key into its components.
+
+    key format: {prefix}_{unique_key}_{checksum}
+
+    Note: Does not validate prefix, checksum, or key.
+
+    returns:
+        prefix: str
+        key: str
+        checksum: str
+    """
+
+    prefix, key, checksum = (
+        api_key.split("_")[0],
+        "_".join(api_key.split("_")[1:-1]),
+        api_key.split("_")[-1],
+    )
+
+    if not prefix or not key or not checksum:
+        raise ValueError("Invalid API key format")
+
+    return prefix, key, checksum
--- src/leapfrogai_api/routers/leapfrogai/auth.py
+++ src/leapfrogai_api/routers/leapfrogai/auth.py
@@ -1,18 +1,26 @@
 """LeapfrogAI endpoints for Auth."""
 
-import hashlib
-import secrets
+import logging
+
+import time
 from fastapi import APIRouter, HTTPException
-from openai import BaseModel
+from pydantic import BaseModel, Field
 from leapfrogai_api.routers.supabase_session import Session
+import leapfrogai_api.backend.security.api_key as security
 
 router = APIRouter(prefix="/leapfrogai/v1/auth", tags=["leapfrogai/auth"])
+
+KEY_PREFIX = "lfai"
 
 
 class CreateAPIKeyRequest(BaseModel):
     """Request body for creating an API key."""
 
-    expires_at: int
+    expires_at: int = Field(
+        default=int(time.time()) + 60 * 60 * 24 * 30,
+        description="The time at which the API key expires, in seconds since the Unix epoch.",
+        examples=[int(time.time()) + 60 * 60 * 24 * 30],  # default to 30 days
+    )
 
 
 @router.post("/create-api-key")
@@ -21,26 +29,45 @@ async def create_api_key(
     request: CreateAPIKeyRequest,
 ) -> dict[str, str]:
     """Create an API key."""
-    # TODO: Generate a more secure API key
-    api_key = hashlib.sha256(secrets.token_bytes(32)).hexdigest()
 
     user_id: str = (await session.auth.get_user()).user.id
 
-    response = (
-        await session.table("api_keys")
-        .insert(
-            {
-                "user_id": user_id,
-                "api_key": api_key,
-                "expires_at": request.expires_at,
-            }
-        )
-        .execute()
-    )
+    api_key = await generate_and_store_api_key(session, user_id, request.expires_at)
 
-    if not response:
+    if not api_key:
         raise HTTPException(status_code=500, detail="Failed to create API key.")
     return {"api_key": api_key}
+
+
+@router.post("/validate-api-key")
+async def validate_api_key(
+    session: Session,
+    api_key: str,
+) -> dict:
+    """Validate an API key."""
+
+    prefix, unique_key, checksum = security.parse(api_key)
+
+    if not prefix == KEY_PREFIX:
+        logging.warning("Received API key with incorrect prefix")
+        return {"valid": False, "details": "Invalid prefix"}
+    if not security.validate_checksum(unique_key=unique_key, checksum=checksum):
+        logging.warning("Received API key with incorrect checksum")
+        return {"valid": False, "details": "Invalid checksum"}
+
+    data, _ = (
+        await session.table("api_keys")
+        .select("*")
+        .eq("api_key", security.encode_unique_key(unique_key=unique_key))
+        .execute()
+    )
+    _, response = data
+
+    if not response:
+        return {"valid": False, "details": "API key not found"}
+    if response[0]["expires_at"] < time.time():
+        return {"valid": False, "details": "API key expired"}
+    return {"valid": True}
 
 
 @router.post("/revoke-api-key")
@@ -49,7 +76,54 @@ async def revoke_api_key(
     api_key: str,
 ) -> dict[str, str]:
     """Revoke an API key."""
-    response = await session.table("api_keys").delete().eq("api_key", api_key).execute()
+
+    _, key, _ = security.parse(api_key)
+
+    stored_key = security.encode_unique_key(unique_key=key)
+
+    response = (
+        await session.table("api_keys").delete().eq("api_key", stored_key).execute()
+    )
     if not response:
         raise HTTPException(status_code=500, detail="Failed to revoke API key.")
     return {"message": "API key revoked."}
+
+
+@router.get("/list-api-keys")
+async def list_api_keys(
+    session: Session,
+) -> dict[str, list[dict]]:
+    """List all API keys."""
+    user_id: str = (await session.auth.get_user()).user.id
+
+    data, _ = (
+        await session.table("api_keys").select("*").eq("user_id", user_id).execute()
+    )
+    _, response = data
+
+    return {"api_keys": response}
+
+
+async def generate_and_store_api_key(
+    session: Session, user_id: str, expires_at: int
+) -> str:
+    """Generate and store an API key."""
+    read_once_token, hashed_token = security.generate_api_key()
+
+    data, _ = (
+        await session.table("api_keys")
+        .insert(
+            {
+                "user_id": user_id,
+                "api_key": hashed_token,
+                "expires_at": expires_at,
+            }
+        )
+        .execute()
+    )
+    _, response = data
+
+    if response:
+        return read_once_token
+
+    raise HTTPException(status_code=500, detail="Failed to store API key.")
