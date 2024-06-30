4e49ba9d00834a9501156335f42b0d38b5dc4259
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-06-30 08:06:24 -0600

    update schema to make sure the key is unique and not expired

--- packages/api/supabase/migrations/20240618163044_api_keys.sql
+++ packages/api/supabase/migrations/20240618163044_api_keys.sql
@@ -2,15 +2,18 @@
 create table api_keys (
     id uuid primary key default uuid_generate_v4(),
     user_id uuid references auth.users not null,
-    api_key text not null,
+    api_key text not null unique,
     created_at bigint default extract(epoch from now()) not null,
     expires_at bigint
 );
 
 alter table api_keys enable row level security;
 
-create policy "Read only if API key matches" ON api_keys for
-    select using (api_key = current_setting('request.headers')::json->>'x-custom-api-key');
+create policy "Read only if API key matches and is current" ON api_keys for
+    select using (
+        api_key = current_setting('request.headers')::json->>'x-custom-api-key'
+        and (expires_at is null or expires_at > extract(epoch from now()))
+    );
 
 create policy "Individuals can view their own api_keys." on api_keys for
     select using (auth.uid() = user_id);
