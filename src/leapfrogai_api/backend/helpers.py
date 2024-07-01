3412c4d9225c9ad73d6ea55e1bd7c998c1fca7a6
John Alling <john.alling@defenseunicorns.com>
2024-07-01 17:34:09 -0400

    set helper to check for None types

--- src/leapfrogai_api/backend/helpers.py
+++ src/leapfrogai_api/backend/helpers.py
@@ -105,7 +105,7 @@ def read_chunks(file: BinaryIO, chunk_size: int) -> Iterator[lfai.AudioRequest]:
 
 
 def object_or_default(obj: Any | None, _default: Any) -> Any:
-    if obj or obj == 0:
+    if obj is not None:
         return obj
     else:
         return _default
