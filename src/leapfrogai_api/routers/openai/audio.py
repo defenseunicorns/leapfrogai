20a2171cb6083c9d673e30691f6ec771cf55dca5
gharvey <gato.harvey@defenseunicorns.com>
2024-07-03 15:36:37 -0700

    Replaces transcription types with translation types

--- src/leapfrogai_api/routers/openai/audio.py
+++ src/leapfrogai_api/routers/openai/audio.py
@@ -4,11 +4,12 @@ from itertools import chain
 from typing import Annotated
 from fastapi import HTTPException, APIRouter, Depends
 from fastapi.security import HTTPBearer
-from leapfrogai_api.backend.grpc_client import create_transcription
+from leapfrogai_api.backend.grpc_client import create_transcription, create_translation
 from leapfrogai_api.backend.helpers import read_chunks
 from leapfrogai_api.backend.types import (
     CreateTranscriptionRequest,
     CreateTranscriptionResponse,
+    CreateTranslationRequest,
 )
 from leapfrogai_api.routers.supabase_session import Session
 from leapfrogai_api.utils import get_model_config
@@ -52,9 +53,9 @@ async def transcribe(
 async def translate(
     session: Session,
     model_config: Annotated[Config, Depends(get_model_config)],
-    req: CreateTranscriptionRequest = Depends(CreateTranscriptionRequest.as_form),
+    req: CreateTranslationRequest = Depends(CreateTranslationRequest.as_form),
 ) -> CreateTranscriptionResponse:
-    """Create a transcription from the given audio file."""
+    """Create a translation to english from the given audio file."""
     model = model_config.get_model_backend(req.model)
     if model is None:
         raise HTTPException(
@@ -63,9 +64,7 @@ async def translate(
         )
 
     # Create a request that contains the metadata for the AudioRequest
-    audio_metadata = lfai.AudioMetadata(
-        prompt=req.prompt, temperature=req.temperature, inputlanguage=req.language
-    )
+    audio_metadata = lfai.AudioMetadata(prompt=req.prompt, temperature=req.temperature)
     audio_metadata_request = lfai.AudioRequest(metadata=audio_metadata)
 
     # Read the file and get an iterator of all the data chunks
@@ -74,4 +73,4 @@ async def translate(
     # combine our metadata and chunk_data iterators
     request_iterator = chain((audio_metadata_request,), chunk_iterator)
 
-    return await create_transcription(model, request_iterator)
+    return await create_translation(model, request_iterator)
