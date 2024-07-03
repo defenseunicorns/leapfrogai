22ccc103358eeb7c91722534ffa8c8e4c9715772
alekst23 <alekst23@gmail.com>
2024-07-03 15:38:05 -0400

    test_modify_thread
    and test_modify_thread_fail

--- tests/unit/leapfrogai_api/routers/openai/test_threads.py
+++ tests/unit/leapfrogai_api/routers/openai/test_threads.py
@@ -1,12 +1,25 @@
 import pytest
 from unittest.mock import patch
+import copy
+from fastapi import HTTPException, status
+
+from openai.types.beta.thread import (
+    ToolResources,
+    ToolResourcesCodeInterpreter,
+    ToolResourcesFileSearch,
+)
+
 from leapfrogai_api.routers.openai.requests.create_thread_request import (
     CreateThreadRequest,
 )
+from leapfrogai_api.backend.types import ModifyThreadRequest
 from leapfrogai_api.data.crud_thread import CRUDThread
 from leapfrogai_api.data.crud_message import CRUDMessage
-
-from leapfrogai_api.routers.openai.threads import create_thread, retrieve_thread
+from leapfrogai_api.routers.openai.threads import (
+    create_thread,
+    retrieve_thread,
+    modify_thread,
+)
 
 from tests.mocks.mock_tables import mock_message, mock_thread
 
@@ -66,19 +79,98 @@ async def test_retrieve_thread(mock_get, mock_session):
     mock_get.assert_called_once_with(filters={"id": mock_thread.id})
 
 
-# @pytest.mark.asyncio
-# @patch.object(CRUDThread, "get")
-# @patch.object(CRUDThread, "update")
-# async def test_modify_thread(mock_update, mock_get, session):
-#     mock_get.return_value = Thread(id="1", object="thread", messages=[])
-#     mock_update.return_value = Thread(id="1", object="thread", messages=[])
+mock_resource_code_interpreter = ToolResourcesCodeInterpreter(file_ids=["mock-data"])
+mock_resource_file_search = ToolResourcesFileSearch(vector_store_ids=["mock-data"])
 
-#     request = ModifyThreadRequest()
-#     response = await router.modify_thread("1", request, session)
 
-#     assert response.id == "1"
-#     assert response.object == "thread"
-#     assert response.messages == []
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "mock_tool_resource",
+    [
+        None,
+        ToolResources(),
+        ToolResources(code_interpreter=None, file_search=mock_resource_file_search),
+    ],
+)
+@patch.object(CRUDThread, "get")
+@patch.object(CRUDThread, "update")
+async def test_modify_thread(
+    mock_thread_update, mock_thread_get, mock_tool_resource, mock_session
+):
+    mock_thread_get.return_value = mock_thread
+
+    mock_thread_update_return = copy.deepcopy(mock_thread)
+    mock_thread_update.return_value = mock_thread_update_return
+
+    mock_metadata = dict(mockfield="mock-data")
+    request = ModifyThreadRequest()
+    request.metadata = mock_thread_update_return.metadata = mock_metadata
+    request.tool_resources = mock_thread_update_return.tool_resources = (
+        mock_tool_resource
+    )
+
+    response = await modify_thread(mock_thread.id, request, mock_session)
+
+    assert response == mock_thread_update_return
+
+    mock_thread_get.assert_called_once_with(filters={"id": mock_thread.id})
+    mock_thread_update.assert_called_once()
+    _, kwargs = mock_thread_update.call_args
+    assert kwargs["object_"].metadata == mock_metadata
+    assert kwargs["object_"].tool_resources == mock_tool_resource
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "mock_tool_resource, mock_get_response, mock_update_response, expected_status_code",
+    [
+        (
+            ToolResources(code_interpreter=mock_resource_code_interpreter),
+            None,
+            None,
+            status.HTTP_400_BAD_REQUEST,
+        ),  # invalid tool resource
+        (
+            ToolResources(code_interpreter=dict(mock="data")),
+            None,
+            None,
+            status.HTTP_400_BAD_REQUEST,
+        ),  # invalid tool resource
+        (
+            ToolResources(),
+            None,
+            None,
+            status.HTTP_404_NOT_FOUND,
+        ),  # no CRUDThread.get response
+        (
+            ToolResources(),
+            mock_thread,
+            None,
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+        ),  # no CRUDThread.update response
+    ],
+)
+@patch.object(CRUDThread, "update")
+@patch.object(CRUDThread, "get")
+async def test_modify_thread_fail(
+    mock_thread_get,
+    mock_thread_update,
+    mock_tool_resource,
+    mock_get_response,
+    mock_update_response,
+    expected_status_code,
+    mock_session,
+):
+    mock_thread_get.return_value = mock_get_response
+    mock_thread_update.return_value = mock_update_response
+
+    request = ModifyThreadRequest()
+    request.tool_resources = mock_tool_resource
+
+    with pytest.raises(HTTPException) as exc_info:
+        await modify_thread(mock_thread.id, request, mock_session)
+
+    assert exc_info.value.status_code == expected_status_code
 
 
 # @pytest.mark.asyncio
