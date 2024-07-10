522d194d821b10e58ddf9e685e4e4c38895c4df7
alekst23 <alekst23@gmail.com>
2024-07-10 16:17:16 -0400

    Cleanup

--- tests/unit/leapfrogai_api/routers/openai/test_runs.py
+++ tests/unit/leapfrogai_api/routers/openai/test_runs.py
@@ -16,15 +16,13 @@ from leapfrogai_api.data.crud_thread import CRUDThread
 from leapfrogai_api.data.crud_assistant import CRUDAssistant
 from leapfrogai_api.data.crud_message import CRUDMessage
 from leapfrogai_api.routers.openai.requests.run_create_params_request import (
-    RunCreateParamsRequestBaseRequest,
+    RunCreateParamsRequest,
 )
 from tests.mocks.mock_tables import mock_run, mock_thread, mock_assistant, mock_message
 
 
 @pytest.mark.asyncio
 @patch("leapfrogai_api.routers.openai.chat.chat_completion")
-# @patch('leapfrogai_api.routers.openai.chat.stream_chat_completion')
-# @patch('leapfrogai_api.routers.openai.chat.stream_chat_completion_raw')
 @patch.object(CRUDMessage, "list")
 @patch.object(CRUDAssistant, "get")
 @patch.object(CRUDRun, "create")
@@ -34,15 +32,9 @@ async def test_create_run(
     mock_run_create,
     mock_assistant_get,
     mock_message_list,
-    # mock_stream_chat_completion_raw,
-    # mock_stream_chat_completion,
     mock_chat_completion,
     mock_session,
 ):
-    # to test: tools, tool_choice
-    # mock: QueryService.query_rag : SingleAPIResponse[RAGResponse]
-    # grpc.aio.insecure_channel(model.backend): grpc.Channel
-    # grpc.aio.UnaryStreamCall.wait_for_connection()
     # Setup
     mock_thread_get.return_value = mock_thread
     mock_run_create.return_value = mock_run
@@ -56,7 +48,7 @@ async def test_create_run(
         choices=[mock_complete_data]
     )
 
-    request = RunCreateParamsRequestBaseRequest()
+    request = RunCreateParamsRequest()
 
     # Test valid run creation
     response = await create_run("thread_id", mock_session, request)
@@ -69,8 +61,6 @@ async def test_create_run(
 
 
 @pytest.mark.asyncio
-# @patch("leapfrogai_api.routers.openai.chat.chat_completion")
-# @patch('leapfrogai_api.routers.openai.chat.stream_chat_completion')
 @patch("leapfrogai_api.routers.openai.chat.stream_chat_completion_raw")
 @patch.object(CRUDMessage, "update")
 @patch.object(CRUDMessage, "list")
@@ -83,9 +73,7 @@ async def test_create_run_stream(
     mock_assistant_get,
     mock_message_list,
     mock_message_update,
-    # mock_stream_chat_completion_raw,
     mock_stream_chat_completion,
-    # mock_chat_completion,
     mock_session,
 ):
     mock_thread_get.return_value = mock_thread
@@ -94,10 +82,7 @@ async def test_create_run_stream(
     mock_message_list.return_value = [mock_message]
     mock_message_update.return_value = mock_message
 
-    request = RunCreateParamsRequestBaseRequest(stream=True)
-
-    # mock_stream_response = StreamingResponse(content=b"mock-data")
-    # mock_stream_chat_completion.return_value = mock_stream_response
+    request = RunCreateParamsRequest(stream=True)
 
     response = await create_run("thread_id", mock_session, request)
     assert isinstance(response, StreamingResponse)
@@ -121,4 +106,6 @@ async def test_create_run_stream(
     mock_thread_get.assert_called_once_with(filters={"id": "thread_id"})
     mock_run_create.assert_called_once()
     mock_stream_chat_completion.assert_called_once()
-    # mock_chat_completion.assert_not_called()
+
+    # TODD: Figure out which aspects of return message to test for
+    assert len(results) == 16
