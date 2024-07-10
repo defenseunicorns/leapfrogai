06ee1b302374b93e65dbe86d5e76ed675f40e930
Andrew Risse <andrewrisse@gmail.com>
2024-07-10 15:16:02 -0600

    Remove name requirement for api key

--- src/leapfrogai_ui/src/lib/schemas/apiKey.ts
+++ src/leapfrogai_ui/src/lib/schemas/apiKey.ts
@@ -2,7 +2,7 @@ import { number, object, type ObjectSchema, string } from 'yup';
 import type { NewApiKeyInput } from '$lib/types/apiKeys';
 
 export const newAPIKeySchema: ObjectSchema<NewApiKeyInput> = object({
-  name: string().required(),
+  name: string(),
   expires_at: number().required()
 })
   .noUnknown(true)
