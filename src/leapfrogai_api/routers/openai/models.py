72dbb68e5d9cc01141a948d83d938fa560872139
gharvey <gato.harvey@defenseunicorns.com>
2024-07-15 14:45:41 -0700

    Fixes model endpoint

--- src/leapfrogai_api/routers/openai/models.py
+++ src/leapfrogai_api/routers/openai/models.py
@@ -16,10 +16,10 @@ router = APIRouter(prefix="/openai/v1/models", tags=["openai/models"])
 @router.get("")
 async def models(
     session: Session,  # pylint: disable=unused-argument # required for authorizing endpoint
-    model_config: Annotated[Config, Depends(get_model_config)],
 ) -> ModelResponse:
     """List all available models."""
-    res = ModelResponse()
+    res = ModelResponse(data=[])
+    model_config: Config = get_model_config()
     for model in model_config.models:
         m = ModelResponseModel(id=model)
         res.data.append(m)
