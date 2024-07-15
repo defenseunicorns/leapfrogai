8ce0f78fb25d4639628fc2bad766b846cafa8e0a
gharvey <gato.harvey@defenseunicorns.com>
2024-07-15 14:55:19 -0700

    Ruff linting

--- src/leapfrogai_api/routers/openai/models.py
+++ src/leapfrogai_api/routers/openai/models.py
@@ -1,7 +1,6 @@
 """OpenAI compliant models router."""
 
-from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from leapfrogai_api.backend.types import (
     ModelResponse,
     ModelResponseModel,
