f47d0ee9b54c8c042e1eecc436bde83e27632bf9
Andrew Risse <andrewrisse@gmail.com>
2024-07-18 11:19:35 -0600

    add navbar with drawer links, needs styling

--- src/leapfrogai_ui/src/lib/components/LFHeader.svelte
+++ src/leapfrogai_ui/src/lib/components/LFHeader.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
-  import { threadsStore, uiStore } from '$stores';
   import logo from '$assets/LeapfrogAI.png';
-  import { Settings, UserAvatar } from 'carbon-icons-svelte';
-  import { Button, Header, HeaderAction, HeaderUtilities } from 'carbon-components-svelte';
-  import { Navbar, NavBrand, NavHamburger } from 'flowbite-svelte';
+  import { Drawer, Navbar, NavBrand } from 'flowbite-svelte';
   import { CogOutline, UserCircleOutline } from 'flowbite-svelte-icons';
+  import IconButton from '$components/IconButton.svelte';
+  import { sineIn } from 'svelte/easing';
 
   export let isUsingOpenAI: boolean;
 
   let loading = false;
   let signOutForm: HTMLFormElement;
-
-  let navClass = 'py-0';
-  let navDivClass = 'flex flex-wrap justify-between items-center w-full';
 
   $: innerWidth = 0;
 
@@ -24,38 +20,106 @@
 
   // We override the default behavior of the HeaderAction component to close other actions
   // when a new one is opened
-  let activeHeaderAction: { [key: string]: boolean };
-  $: activeHeaderAction = {
-    user: false,
-    settings: false
+  let headerActionHidden: { [key: string]: boolean };
+  $: headerActionHidden = {
+    user: true,
+    settings: true
   };
 
-  const setActiveHeaderAction = (headerActionName: keyof typeof activeHeaderAction) => {
-    Object.keys(activeHeaderAction).forEach((key) => {
-      activeHeaderAction[key] = key === headerActionName;
+  const openDrawer = (headerActionName: keyof typeof headerActionHidden) => {
+    Object.keys(headerActionHidden).forEach((key) => {
+      if (key === headerActionName) {
+        headerActionHidden[key] = false;
+      } else {
+        headerActionHidden[key] = true;
+      }
     });
+  };
+
+  let transitionParams = {
+    x: 320,
+    duration: 200,
+    easing: sineIn
   };
 </script>
 
 <svelte:window bind:innerWidth />
 
 <header>
-  <Navbar {navClass} {navDivClass} class="dark:bg-secondary-500">
+  <Navbar fluid class="dark:bg-secondary-500 dark:border-primary-400 h-12 py-1 dark:border-b">
     <NavBrand href="/chat">
       <img src={logo} class="w-[7.875rem]] h-[2.25rem]" alt="LeapfrogAI Logo" />
     </NavBrand>
-    <div class="flex items-center lg:order-2">
-      <Button>
+    <div class="flex items-center gap-x-2">
+      <IconButton on:click={() => openDrawer('settings')}>
         <CogOutline data-testid="header-settings-btn" />
-      </Button>
-      <Button>
-        <UserCircleOutline data-testid="header-profile-btn" />
-      </Button>
+      </IconButton>
 
-      <NavHamburger
-        on:click={toggle}
-        btnClass="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-      />
+      <IconButton on:click={() => openDrawer('user')}>
+        <UserCircleOutline data-testid="header-user-btn" />
+      </IconButton>
     </div>
   </Navbar>
+
+  <Drawer
+    transitionType="fly"
+    {transitionParams}
+    bind:hidden={headerActionHidden.settings}
+    placement="right"
+    id="settings-drawer"
+  >
+    <div class="links-container">
+      <a href="/chat/assistants-management" class="header-link" on:click={() => openDrawer('')}
+        >Assistants Management</a
+      >
+
+      <a href="/chat/file-management" class="header-link" on:click={() => openDrawer('')}
+        >File Management</a
+      >
+      {#if !isUsingOpenAI}
+        <a href="/chat/api-keys" class="header-link" on:click={() => openDrawer('')}>API Keys</a>
+      {/if}
+    </div>
+  </Drawer>
+  <Drawer
+    transitionType="fly"
+    {transitionParams}
+    bind:hidden={headerActionHidden.user}
+    placement="right"
+    id="user-drawer"
+  >
+    <div class="links-container">
+      <form bind:this={signOutForm} method="post" action="/auth?/signout">
+        <button class="header-link" aria-label="Log Out" disabled={loading} on:click={handleLogOut}
+          >Log Out</button
+        >
+      </form>
+    </div>
+  </Drawer>
 </header>
+
+<style lang="scss">
+  .links-container {
+    display: flex;
+    padding: 1rem;
+    flex-direction: column;
+    gap: 0.88rem;
+  }
+
+  .header-link {
+    font-size: 0.875rem;
+    line-height: 1.125rem;
+    font-weight: 600;
+    letter-spacing: 0.16px;
+    cursor: pointer;
+    background: none;
+    color: inherit;
+    border: none;
+    padding: 0;
+    outline: inherit;
+    text-decoration: none;
+    &:hover {
+      color: #ffffff;
+    }
+  }
+</style>
