aa22063af1a7aa80975f70c69dbef196e4f82601
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-07-22 18:11:34 -0400

    remove the slash

--- src/leapfrogai_api/routers/leapfrogai/models.py
+++ src/leapfrogai_api/routers/leapfrogai/models.py
@@ -4,7 +4,7 @@ from leapfrogai_api.utils import get_model_config
 router = APIRouter(prefix="/leapfrogai/v1/models", tags=["leapfrogai/models"])
 
 
-@router.get("/")
+@router.get("")
 async def models():
     """List all the models."""
     return get_model_config()
