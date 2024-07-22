cf5e6a04e921238ba38bc5a2a6bd147e748601a4
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-07-22 18:54:42 -0400

    updates to the RAG endpoint naming

--- src/leapfrogai_api/main.py
+++ src/leapfrogai_api/main.py
@@ -11,7 +11,7 @@ from fastapi.exceptions import RequestValidationError
 from leapfrogai_api.routers.base import router as base_router
 from leapfrogai_api.routers.leapfrogai import auth
 from leapfrogai_api.routers.leapfrogai import models as lfai_models
-from leapfrogai_api.routers.leapfrogai import rag
+from leapfrogai_api.routers.leapfrogai import vector_stores as lfai_vector_stores
 from leapfrogai_api.routers.openai import (
     assistants,
     audio,
@@ -69,7 +69,7 @@ app.include_router(vector_stores.router)
 app.include_router(runs.router)
 app.include_router(messages.router)
 app.include_router(runs_steps.router)
-app.include_router(rag.router)
+app.include_router(lfai_vector_stores.router)
 app.include_router(lfai_models.router)
 # This should be at the bottom to prevent it preempting more specific runs endpoints
 # https://fastapi.tiangolo.com/tutorial/path-params/#order-matters
