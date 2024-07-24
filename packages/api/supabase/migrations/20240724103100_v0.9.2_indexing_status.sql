7c876046b4c6d241b16d14f89172cade520d46c7
gharvey <gato.harvey@defenseunicorns.com>
2024-07-24 13:54:17 -0700

    Simplifies sql query to focus on vector_store_file

--- packages/api/supabase/migrations/20240724103100_v0.9.2_indexing_status.sql
+++ packages/api/supabase/migrations/20240724103100_v0.9.2_indexing_status.sql
@@ -1,35 +1,8 @@
--- Create a table to store the status of the vector store file indexing
-CREATE TABLE vector_store_file_status (
-    id VARCHAR PRIMARY KEY,
-    user_id uuid references auth.users not null,
-    status varchar default 'in_progress' not null,
-    created_at bigint default extract(epoch from now()) not null,
-    updated_at bigint default extract(epoch from now()) not null
-);
+-- Update the vector_store_file table to add an updated_at column
+alter table vector_store_file add column updated_at bigint default extract(epoch from now()) not null;
 
 -- Add an index on user_id for faster queries
-CREATE INDEX idx_vector_store_file_status_user_id ON vector_store_file_status(user_id);
-
--- Turn on security
-alter table vector_store_file_status enable row level security;
-
--- Allow users to CRUD their own vector_store_file_status via API key.
-create policy "Individuals can CRUD their own vector_store_file_status via API key."
-    on vector_store_file_status for all
-    to anon
-    using
-    (
-        exists (
-            select 1
-            from api_keys
-            where api_keys.api_key_hash = crypt(current_setting('request.headers')::json->>'x-custom-api-key', api_keys.api_key_hash)
-            and api_keys.user_id = vector_store_file_status.user_id
-        )
-    );
-
--- Allow users to CRUD their own vector_store_file_status
-create policy "Individuals can CRUD their own vector_store_file_status" on vector_store_file_status for
-    all to anon using (auth.uid() = user_id);
+CREATE INDEX idx_vector_store_file_user_id ON vector_store_file(user_id);
 
 -- Create a function to update the updated_at column
 CREATE OR REPLACE FUNCTION update_modified_column()
@@ -41,11 +14,11 @@ END;
 $$ language 'plpgsql';
 
 -- Create a trigger to automatically update the updated_at column
-CREATE TRIGGER update_vector_store_file_status_modtime
-BEFORE UPDATE ON vector_store_file_status
+CREATE TRIGGER update_vector_store_file_modtime
+BEFORE UPDATE ON vector_store_file
 FOR EACH ROW
 EXECUTE FUNCTION update_modified_column();
 
--- Enable Supabase realtime for the vector_store_file_status table
+-- Enable Supabase realtime for the vector_store_file table
 alter publication supabase_realtime
-add table vector_store_file_status;
+add table vector_store_file;
