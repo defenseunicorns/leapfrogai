f94f10977f08624f7652a6b97b724a6f742dae4c
gharvey <gato.harvey@defenseunicorns.com>
2024-07-25 15:41:35 -0700

    Conditionally apply args

--- packages/whisper/main.py
+++ packages/whisper/main.py
@@ -18,7 +18,14 @@ def make_transcribe_request(filename, task, language, temperature, prompt):
     device = "cuda" if GPU_ENABLED else "cpu"
     model = WhisperModel(model_path, device=device, compute_type="float32")
 
-    segments, info = model.transcribe(filename, task=task, beam_size=5, temperature=temperature, initial_prompt=prompt, language=language)
+    segments, info = model.transcribe(
+        filename,
+        task=task,
+        beam_size=5,
+        temperature=temperature,
+        initial_prompt=prompt,
+        language=language,
+    )
 
     output = ""
 
