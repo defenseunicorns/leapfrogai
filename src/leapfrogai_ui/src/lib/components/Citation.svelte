127364916e6e72585cf4a4655449d52940f61e2f
Andrew Risse <andrewrisse@gmail.com>
2024-08-07 10:13:11 -0600

    init

--- src/leapfrogai_ui/src/lib/components/Citation.svelte
+++ src/leapfrogai_ui/src/lib/components/Citation.svelte
@@ -3,8 +3,12 @@
   import { toastStore } from '$stores';
   import type { FileObject } from 'openai/resources/files';
   import { ArrowUpOutline } from 'flowbite-svelte-icons';
+  import { onDestroy } from 'svelte';
   export let file: FileObject;
   export let index: string;
+
+  let expanded = false;
+  let url: string;
 
   const handleClick = async () => {
     if (browser) {
@@ -19,31 +23,41 @@
       }
 
       const blob = await res.blob();
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.style.display = 'none';
-      a.href = url;
-      a.download = file.filename;
-      document.body.appendChild(a);
-      a.click();
+      url = window.URL.createObjectURL(blob);
+      // const a = document.createElement('a');
+      // a.style.display = 'none';
+      // a.href = url;
+      // a.download = file.filename;
+      // document.body.appendChild(a);
+      // a.click();
+      //
+      // if (res.headers.get('content-type') === 'application/pdf') {
+      //   // Open in a new tab
+      //   window.open(url, '_blank');
+      // }
 
-      if (res.headers.get('content-type') === 'application/pdf') {
-        // Open in a new tab
-        window.open(url, '_blank');
-      }
-
-      window.URL.revokeObjectURL(url); // avoid memory leaks
+      // window.URL.revokeObjectURL(url); // avoid memory leaks
     }
   };
+
+  onDestroy(() => {
+    if (url) window.URL.revokeObjectURL(url); // avoid memory leaks
+  });
 </script>
 
 <button
   class="flex items-center justify-center gap-1"
   on:click={(e) => {
     e.preventDefault();
+    expanded = !expanded;
     handleClick();
   }}
 >
   <span class="link">{`[${index}] ${file.filename}`}</span>
   <ArrowUpOutline class="link cursor-pointer" color="#78a9ff" />
 </button>
+{#if expanded && url}
+  <div class="relative h-0 w-full pb-[33%]">
+    <iframe src={url} class="absolute left-0 top-0 h-full w-full border-0" />
+  </div>
+{/if}
