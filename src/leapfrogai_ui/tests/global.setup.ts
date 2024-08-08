cd4200932889c164ac466ec864010a0757fd0c36
Andrew Risse <andrewrisse@gmail.com>
2024-08-08 16:21:08 -0600

    Remove setup/teardown

--- src/leapfrogai_ui/tests/global.setup.ts
+++ src/leapfrogai_ui/tests/global.setup.ts
@@ -1,14 +1,10 @@
 import { test as setup } from './fixtures';
 import * as OTPAuth from 'otpauth';
 import { delay } from 'msw';
-import { deleteAllGeneratedFixtureFiles, deleteAllTestFilesWithApi } from './helpers/fileHelpers';
-import { deleteAllAssistants, deleteAssistantAvatars } from './helpers/assistantHelpers';
-import { deleteAllTestThreadsWithApi } from './helpers/threadHelpers';
-import { deleteAllTestAPIKeys } from './helpers/apiHelpers';
 
 const authFile = 'playwright/.auth/user.json';
 
-setup('authenticate', async ({ page, openAIClient }) => {
+setup('authenticate', async ({ page }) => {
   await page.goto('/'); // go to the home page
   await delay(2000); // allow page to fully hydrate
   if (process.env.PUBLIC_DISABLE_KEYCLOAK === 'true') {
@@ -68,13 +64,4 @@ setup('authenticate', async ({ page, openAIClient }) => {
   // End of authentication steps.
 
   await page.context().storageState({ path: authFile });
-
-  console.log('setting up...');
-  deleteAllGeneratedFixtureFiles();
-  await deleteAllTestFilesWithApi(openAIClient);
-  await deleteAllAssistants(openAIClient);
-  await deleteAllTestThreadsWithApi(openAIClient);
-  await deleteAssistantAvatars();
-  await deleteAllTestAPIKeys();
-  console.log('set up complete');
 });
