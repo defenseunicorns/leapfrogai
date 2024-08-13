79e09a28961ac95a285723131a93a5f976c1e9c8
gharvey <gato.harvey@defenseunicorns.com>
2024-08-13 11:37:44 -0700

    Removes explicit install of requests dep and adds new realtime dep

--- tests/e2e/test_supabase.py
+++ tests/e2e/test_supabase.py
@@ -1,4 +1,5 @@
 import requests
+from realtime.connection import Socket
 
 from .utils import ANON_KEY
 
@@ -18,3 +19,16 @@ def test_studio():
         print(f"Error: Request failed with status code {response.status_code}")
         print(e)
         exit(1)
+
+
+def test_supabase_realtime_vector_store_indexing():
+    def callback1(payload):
+        print("Callback 1: ", payload)
+
+    URL = f"wss://supabase-kong.uds.dev/realtime/v1/?apikey={ANON_KEY}&vsn=1.0.0"
+    s = Socket(URL)
+    s.connect()
+
+    channel_1 = s.set_channel("realtime:*")
+    channel_1.join().on("UPDATE", callback1)
+    s.listen()
