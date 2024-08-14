8c0bc36c43402f1a7e5a4a4858681e67d530a154
Andrew Risse <andrewrisse@gmail.com>
2024-08-14 14:22:13 -0600

    init

--- src/leapfrogai_ui/src/routes/+layout.svelte
+++ src/leapfrogai_ui/src/routes/+layout.svelte
@@ -5,14 +5,40 @@
   import { Toasts } from '$components';
   import { page } from '$app/stores';
   import '$webComponents/CodeBlock';
+  import {browser} from "$app/environment";
   export let data;
 
   let { supabase, session } = data;
 
   $: ({ supabase, session } = data);
 
+  let resetTimer = true;
+  $: {
+    if (resetTimer && browser) {
+      console.log("setting refresh timeout")
+      resetTimer = false;
+      setTimeout(async () => {
+        // const { data, error } = await supabase.auth.refreshSession();
+        invalidate('supabase:auth');
+        if (error) {
+          console.error('Error refreshing session:', error);
+        } else {
+          console.log('Session refreshed:', data);
+        }
+        resetTimer = true
+      }, 10 * 1000);
+    }
+  }
+
+
   onMount(() => {
-    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+    const { data } = supabase.auth.onAuthStateChange((event, newSession) => {
+      // console.log('auth state change');
+      // console.log('new session expires at', newSession?.expires_at);
+      // console.log('session expires at', session?.expires_at);
+      if(event === "TOKEN_REFRESHED"){
+        return;
+      }
       if (newSession?.expires_at !== session?.expires_at) {
         invalidate('supabase:auth');
       }
