eedff06500952fba2da8b04becc62ed1cc6d021f
Andrew Risse <andrewrisse@gmail.com>
2024-08-15 15:12:03 -0600

    use make command and cleanup

--- .github/scripts/createUser.sh
+++ .github/scripts/createUser.sh
@@ -1,13 +1,10 @@
 KEYCLOAK_ADMIN_PASSWORD=$(uds zarf tools kubectl get secret -n keycloak keycloak-admin-password -o jsonpath={.data.password} | base64 -d)
-echo $KEYCLOAK_ADMIN_PASSWORD
 KEYCLOAK_ADMIN_TOKEN=$(curl -s --location "https://keycloak.admin.uds.dev/realms/master/protocol/openid-connect/token" \
 --header "Content-Type: application/x-www-form-urlencoded" \
 --data-urlencode "username=admin" \
 --data-urlencode "password=${KEYCLOAK_ADMIN_PASSWORD}" \
 --data-urlencode "client_id=admin-cli" \
 --data-urlencode "grant_type=password" | uds zarf tools yq .access_token)
-echo "Passed Admin token step"
-echo $KEYCLOAK_ADMIN_TOKEN
 curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/users" \
 --header "Content-Type: application/json" \
 --header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" \
@@ -30,10 +27,8 @@ curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/users" \
     }
   ]
 }'
-echo "Passed create user step"
 CONDITIONAL_OTP_ID=$(curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/authentication/flows/Authentication/executions" \
 --header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" | uds zarf tools yq '.[] | select(.displayName == "Conditional OTP") | .id')
-echo "Passed conditional OTP step"
 curl --location --request PUT "https://keycloak.admin.uds.dev/admin/realms/uds/authentication/flows/Authentication/executions" \
 --header "Content-Type: application/json" \
 --header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" \
@@ -41,4 +36,4 @@ curl --location --request PUT "https://keycloak.admin.uds.dev/admin/realms/uds/a
 \"id\": \"${CONDITIONAL_OTP_ID}\",
 \"requirement\": \"DISABLED\"
 }"
-echo "Passed all steps"
+echo "User Created"
