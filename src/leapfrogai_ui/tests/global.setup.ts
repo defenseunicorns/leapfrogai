464ddbd104aaf6b7bb11da6cbd26c18ea811a5f9
Andrew Risse <52644157+andrewrisse@users.noreply.github.com>
2024-08-22 08:12:02 -0600

    fix(ui): playwright logout causes others to fail (#923)

--- src/leapfrogai_ui/tests/global.setup.ts
+++ src/leapfrogai_ui/tests/global.setup.ts
@@ -1,60 +1,95 @@
-import { test as setup } from './fixtures';
+import { expect, test as setup } from './fixtures';
 import * as OTPAuth from 'otpauth';
 import { delay } from 'msw';
+import type { Page } from '@playwright/test';
+import { cleanup } from './helpers/cleanup';
 
 const authFile = 'playwright/.auth/user.json';
 
-setup('authenticate', async ({ page }) => {
+const doSupabaseLogin = async (page: Page) => {
+  await page.goto('/'); // go to the home page
+  await delay(2000); // allow page to fully hydrate
+  // when running in Github CI, create a new account because we don't have seed migrations
+  const emailField = page.getByTestId('email-input');
+  const passwordField = page.getByTestId('password-input');
+
+  await emailField.click();
+  await emailField.fill(process.env.USERNAME!);
+  await passwordField.click();
+  await passwordField.fill(process.env.PASSWORD!);
+
+  const emailText = await emailField.innerText();
+  const passwordText = await passwordField.innerText();
+  if (emailText !== process.env.USERNAME!) await emailField.fill(process.env.USERNAME!);
+  if (passwordText !== process.env.PASSWORD!) await passwordField.fill(process.env.PASSWORD!);
+
+  await page.getByTestId('submit-btn').click();
+
+  await delay(2000);
+  const userExists = (await page.getByText('User already exists').count()) > 0;
+  if (userExists) {
+    await page.getByTestId('toggle-submit-btn').click();
+    await page.getByTestId('submit-btn').click();
+  }
+  // }
+};
+
+const doKeycloakLogin = async (page: Page) => {
+  await page.goto('/'); // go to the home page
+  await delay(2000); // allow page to fully hydrate
+  // With Keycloak
+  await page.getByRole('button', { name: 'Log In' }).click();
+  await page.getByLabel('Username or email').fill(process.env.USERNAME!);
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill(process.env.PASSWORD!);
+  await page.getByRole('button', { name: 'Log In' }).click();
+
+  const totp = new OTPAuth.TOTP({
+    issuer: 'Unicorn Delivery Service',
+    algorithm: 'SHA1',
+    digits: 6,
+    period: 30,
+    secret: process.env.MFA_SECRET!
+  });
+  const code = totp.generate();
+  await page.getByLabel('Six digit code').fill(code);
+  await page.getByRole('button', { name: 'Log In' }).click();
+};
+
+const login = async (page: Page) => {
+  if (process.env.PUBLIC_DISABLE_KEYCLOAK === 'true') {
+    await doSupabaseLogin(page);
+  } else {
+    await doKeycloakLogin(page);
+  }
+};
+
+const logout = async (page: Page) => {
+  await expect(page).toHaveTitle('LeapfrogAI - Chat');
+  await page.getByTestId('header-profile-btn').click();
+  await page
+    .getByTestId('profile-dropdown')
+    .getByRole('button', { name: /log out/i })
+    .click();
+
+  await page.waitForURL('/');
+
+  if (process.env.PUBLIC_DISABLE_KEYCLOAK === 'true') {
+    await expect(page.getByText('Sign In')).toBeVisible();
+  } else {
+    const loginBtn = page.getByRole('button', { name: /Log In with UDS SSO/i });
+    await expect(loginBtn).toBeVisible();
+    await loginBtn.click();
+    await page.waitForURL(`**/*/realms/uds/**/*`); // ensure full logout of keycloak (not just supabase)
+  }
+};
+
+setup('authenticate', async ({ page, openAIClient }) => {
   page.on('pageerror', (err) => {
     console.log(err.message);
   });
 
-  await page.goto('/'); // go to the home page
-  await delay(2000); // allow page to fully hydrate
-  if (process.env.PUBLIC_DISABLE_KEYCLOAK === 'true') {
-    // when running in Github CI, create a new account because we don't have seed migrations
-    const emailField = page.getByTestId('email-input');
-    const passwordField = page.getByTestId('password-input');
-    if (process.env.TEST_ENV === 'CI') {
-      await emailField.click();
-      await emailField.fill('ci_user@test.com');
-      await passwordField.click();
-      await passwordField.fill('password123');
-
-      const emailText = await emailField.innerText();
-      const passwordText = await passwordField.innerText();
-      if (emailText !== 'ci_user@test.com') await emailField.fill('ci_user@test.com');
-      if (passwordText !== 'password123') await passwordField.fill('password123');
-
-      await page.getByTestId('submit-btn').click();
-    } else {
-      // uses local supabase test users, logs in directly with Supabase, no Keycloak
-      await page.getByTestId('toggle-submit-btn').click();
-      await emailField.click();
-      await emailField.fill('user1@test.com');
-      await passwordField.click();
-      await passwordField.fill('password123');
-      await page.getByTestId('submit-btn').click();
-    }
-  } else {
-    // With Keycloak
-    await page.getByRole('button', { name: 'Log In' }).click();
-    await page.getByLabel('Username or email').fill(process.env.USERNAME!);
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill(process.env.PASSWORD!);
-    await page.getByRole('button', { name: 'Log In' }).click();
-
-    const totp = new OTPAuth.TOTP({
-      issuer: 'Unicorn Delivery Service',
-      algorithm: 'SHA1',
-      digits: 6,
-      period: 30,
-      secret: process.env.MFA_SECRET!
-    });
-    const code = totp.generate();
-    await page.getByLabel('Six digit code').fill(code);
-    await page.getByRole('button', { name: 'Log In' }).click();
-  }
+  await login(page);
 
   // Wait until the page receives the cookies.
   //
@@ -62,10 +97,26 @@ setup('authenticate', async ({ page }) => {
   // Wait for the final URL to ensure that the cookies are actually set.
   await page.waitForURL('/chat');
 
+  if (!process.env.SKIP_LOGOUT_TEST) {
+    // First test log out, then log back in and continue. We don't want to put logout in its own test, because it
+    // will invalidate the session and cause other tests to fail
+    await logout(page);
+
+    if (process.env.PUBLIC_DISABLE_KEYCLOAK === 'false') await delay(31000); // prevent logging back in too quickly and getting denied
+    // Log back in to begin rest of tests
+    await login(page);
+
+    await page.waitForURL('/chat');
+  }
+
   // Alternatively, you can wait until the page reaches a state where all cookies are set.
   //   await expect(page.getByRole('button', { name: 'View profile and more' })).toBeVisible();
 
   // End of authentication steps.
 
   await page.context().storageState({ path: authFile });
+
+  if (process.env.TEST_ENV !== 'CI') {
+    await cleanup(openAIClient);
+  }
 });
