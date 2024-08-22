d38b689a21eb55fe1624bfabb9f5eb31e51ddc5f
Andrew Risse <andrewrisse@gmail.com>
2024-08-22 10:07:11 -0500

    new debug 1

--- .github/scripts/createUser.sh
+++ .github/scripts/createUser.sh
@@ -1,3 +1,4 @@
+echo Echo test fakeE2EUserPassword: $fakeE2EUserPassword
 KEYCLOAK_ADMIN_PASSWORD=$(uds zarf tools kubectl get secret -n keycloak keycloak-admin-password -o jsonpath={.data.password} | base64 -d)
 echo "::add-mask::$KEYCLOAK_ADMIN_PASSWORD"
 
@@ -10,8 +11,6 @@ KEYCLOAK_ADMIN_TOKEN=$(curl --location "https://keycloak.admin.uds.dev/realms/ma
 --data-urlencode "grant_type=password" | uds zarf tools yq .access_token)
 echo "::add-mask::$KEYCLOAK_ADMIN_TOKEN"
 
-echo Echo test KEYCLOAK_ADMIN_PASSWORD: $KEYCLOAK_ADMIN_PASSWORD
-echo Echo test KEYCLOAK_ADMIN_TOKEN: $KEYCLOAK_ADMIN_TOKEN
 
 curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/users" \
 --http1.1 \
