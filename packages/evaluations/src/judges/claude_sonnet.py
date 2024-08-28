15ae77ce1a9844d2fbaaaac7cd3275a530ab388a
John Alling <john.alling@defenseunicorns.com>
2024-08-28 14:01:14 -0400

    add claude llm judge model

---
+++ packages/evaluations/src/judges/claude_sonnet.py
@@ -0,0 +1,53 @@
+import os
+
+import instructor
+from pydantic import BaseModel
+from deepeval.models.base_model import DeepEvalBaseLLM
+import asyncio
+from anthropic import Anthropic
+from typing import Optional
+
+
+class ClaudeSonnet(DeepEvalBaseLLM):
+    """A DeepEval LLM class that utilizes the Anthropic API to utilize Claude models"""
+
+    def __init__(
+        self, api_key: Optional[str] = None, model: str = "claude-3-5-sonnet-20240620"
+    ):
+        self.model = model
+        self.client = Anthropic(api_key=api_key or os.environ.get("ANTHROPIC_API_KEY"))
+
+    def load_model(self):
+        """Returns the current model selected"""
+        return self.model
+
+    def generate(
+        self,
+        prompt: str,
+        schema: BaseModel,
+        max_tokens: int = 1024,
+    ) -> str:
+        instructor_client = instructor.from_anthropic(self.client)
+        resp = instructor_client.messages.create(
+            model=self.model,
+            max_tokens=max_tokens,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            response_model=schema,
+        )
+        return resp
+
+    async def a_generate(
+        self, prompt: str, schema: BaseModel, max_tokens: Optional[int]
+    ) -> str:
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(
+            None, self.generate, prompt, schema, max_tokens
+        )
+
+    def get_model_name(self):
+        return f"Anthropic {self.model}"
