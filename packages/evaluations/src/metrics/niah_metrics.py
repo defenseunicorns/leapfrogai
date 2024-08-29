6d39012489a690f34343e6895746d7fd1f930a95
John Alling <john.alling@defenseunicorns.com>
2024-08-29 10:06:43 -0400

    update metric docstrings and simplify metrics

--- packages/evaluations/src/metrics/niah_metrics.py
+++ packages/evaluations/src/metrics/niah_metrics.py
@@ -15,15 +15,32 @@ class NIAH_Retrieval(BaseMetric):
         self.async_mode = async_mode
 
     def measure(self, test_case: LLMTestCase) -> int:
-        """Records the niah retrieval score from the test case"""
-        self.success = (
-            test_case.additional_metadata["retrieval_score"] >= self.threshold
-        )
+        """
+        Records the niah retrieval score from the test case
+
+        This function checks for the presence of a retrieval_score (provided by the niah_runner)
+        and sets a boolean determined by said score. The score is calculated in the runner to keep the
+        runner self-contained as a means of running the entire evaluation on its own. For simplicity,
+        the score is copied here for integration with DeepEval.
+
+        params:
+        -------
+        test_case: LLMTestCase
+            A test case object built from the results of a needle in a haystack evaluation run.
+            test_case should contain an additional metadata field that returns a dictionary with
+            the field "retrieval_score"
+
+        returns:
+        -------
+        int
+            A score that is equal to the "retrieval_score" from the test_case
+        """
+        self.score = test_case.additional_metadata["retrieval_score"]
+        self.success = self.score >= self.threshold
+
         if self.success:
-            self.score = 1
             self.reason = f"Retrieval in the NIAH evaluation scored greater than or equal to the threshold score of {self.threshold}"
         else:
-            self.score = 0
             self.reason = f"Retrieval in the NIAH evaluation scored less than the threshold score of {self.threshold}"
 
         return self.score
@@ -52,13 +69,32 @@ class NIAH_Response(BaseMetric):
         self.async_mode = async_mode
 
     def measure(self, test_case: LLMTestCase) -> int:
-        """Records the niah response score from the test case"""
-        self.success = test_case.additional_metadata["response_score"] >= self.threshold
+        """
+        Records the niah response score from the test case
+
+        This function checks for the presence of a response_score (provided by the niah_runner)
+        and sets a boolean determined by said score. The score is calculated in the runner to keep the
+        runner self-contained as a means of running the entire evaluation on its own. For simplicity,
+        the score is copied here for integration with DeepEval.
+
+        params:
+        -------
+        test_case: LLMTestCase
+            A test case object built from the results of a needle in a haystack evaluation run.
+            test_case should contain an additional metadata field that returns a dictionary with
+            the field "response_score"
+
+        returns:
+        -------
+        int
+            A score that is equal to the "response_score" from the test_case
+        """
+        self.score = test_case.additional_metadata["response_score"]
+        self.success = self.score >= self.threshold
+
         if self.success:
-            self.score = 1
             self.reason = f"Response in the NIAH evaluation scored greater than or equal to the threshold score of {self.threshold}"
         else:
-            self.score = 0
             self.reason = f"Response in the NIAH evaluation scored less than the threshold score of {self.threshold}"
 
         return self.score
