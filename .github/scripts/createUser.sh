2ac0cf8393fbd5136b7b5fcd0013fa3266dd15f3
Justin Law <justin.law@defenseunicorns.com>
2024-08-30 12:29:18 -0400

    revert merge from remote branch

---
+++ .github/scripts/createUser.sh
@@ -0,0 +1,57 @@
+KEYCLOAK_ADMIN_PASSWORD=$(uds zarf tools kubectl get secret -n keycloak keycloak-admin-password -o jsonpath={.data.password} | base64 -d)
+echo "::add-mask::$KEYCLOAK_ADMIN_PASSWORD"
+
+KEYCLOAK_ADMIN_TOKEN=$(curl --location "https://keycloak.admin.uds.dev/realms/master/protocol/openid-connect/token" \
+	--http1.1 \
+	--header "Content-Type: application/x-www-form-urlencoded" \
+	--data-urlencode "username=admin" \
+	--data-urlencode "password=${KEYCLOAK_ADMIN_PASSWORD}" \
+	--data-urlencode "client_id=admin-cli" \
+	--data-urlencode "grant_type=password" | uds zarf tools yq .access_token)
+echo "::add-mask::$KEYCLOAK_ADMIN_TOKEN"
+
+curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/users" \
+	--http1.1 \
+	--header "Content-Type: application/json" \
+	--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" \
+	--data "{
+  \"username\": \"doug\",
+  \"firstName\": \"Doug\",
+  \"lastName\": \"Unicorn\",
+  \"email\": \"doug@uds.dev\",
+  \"attributes\": {
+    \"mattermostid\": \"1\"
+  },
+  \"emailVerified\": true,
+  \"enabled\": true,
+  \"requiredActions\": [],
+  \"credentials\": [
+    {
+      \"type\": \"password\",
+      \"value\": \"${FAKE_E2E_USER_PASSWORD}\",
+      \"temporary\": false
+    }
+  ]
+}"
+
+CONDITIONAL_OTP_ID=$(curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/authentication/flows/Authentication/executions" \
+	--http1.1 \
+	--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" | uds zarf tools yq '.[] | select(.displayName == "Conditional OTP") | .id')
+
+curl --location --request PUT "https://keycloak.admin.uds.dev/admin/realms/uds/authentication/flows/Authentication/executions" \
+	--http1.1 \
+	--header "Content-Type: application/json" \
+	--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" \
+	--data "{
+\"id\": \"${CONDITIONAL_OTP_ID}\",
+\"requirement\": \"DISABLED\"
+}"
+
+USER=$(
+	curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/users?user=doug" \
+		--http1.1 \
+		--header "Content-Type: application/json" \
+		--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}"
+)
+
+echo "User: $USER"
