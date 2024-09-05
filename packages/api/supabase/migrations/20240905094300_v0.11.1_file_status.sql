d4bd6ed93e508bd4f98a0249ac74e48b2eba01b6
gharvey <gato.harvey@defenseunicorns.com>
2024-09-05 10:50:03 -0700

    Removes unnecessary function definition

--- packages/api/supabase/migrations/20240905094300_v0.11.1_file_status.sql
+++ packages/api/supabase/migrations/20240905094300_v0.11.1_file_status.sql
@@ -1,15 +1,6 @@
 -- Update the file_objects table to add an updated_at column
 ALTER TABLE file_objects ADD COLUMN updated_at timestamp DEFAULT timezone('utc', now()) NOT NULL;
 
--- Create a function to update the updated_at column
-CREATE OR REPLACE FUNCTION update_modified_column()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = timezone('utc', now());
-    RETURN NEW;
-END;
-$$ language 'plpgsql';
-
 -- Create a trigger to automatically update the updated_at column
 CREATE TRIGGER update_file_objects_modtime
 BEFORE UPDATE ON file_objects
