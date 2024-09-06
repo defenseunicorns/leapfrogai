08a1af59182a4f1eaf46a9fbb76dbb789b8583ab
alekst23 <alekst23@gmail.com>
2024-09-06 13:17:49 -0400

    fixes from comments

--- src/leapfrogai_api/typedef/constants.py
+++
@@ -1,6 +0,0 @@
-# This file defines application constants not set by environment variables or configuration files.
-
-THIRTY_DAYS = 60 * 60 * 24 * 30  # in seconds
-
-DEFAULT_MAX_COMPLETION_TOKENS = 4096
-DEFAULT_MAX_PROMPT_TOKENS = 4096
