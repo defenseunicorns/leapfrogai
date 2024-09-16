ee8f15149a21cbd6786bc456e7dd9abbbca2179d
Justin Law <justin.law@defenseunicorns.com>
2024-09-16 14:46:25 -0400

    revert formatting changes outside of scope

--- .github/scripts/createUser.sh
+++ .github/scripts/createUser.sh
@@ -2,19 +2,20 @@ KEYCLOAK_ADMIN_PASSWORD=$(uds zarf tools kubectl get secret -n keycloak keycloak
 echo "::add-mask::$KEYCLOAK_ADMIN_PASSWORD"
 
 KEYCLOAK_ADMIN_TOKEN=$(curl --location "https://keycloak.admin.uds.dev/realms/master/protocol/openid-connect/token" \
-	--http1.1 \
-	--header "Content-Type: application/x-www-form-urlencoded" \
-	--data-urlencode "username=admin" \
-	--data-urlencode "password=${KEYCLOAK_ADMIN_PASSWORD}" \
-	--data-urlencode "client_id=admin-cli" \
-	--data-urlencode "grant_type=password" | uds zarf tools yq .access_token)
+--http1.1 \
+--header "Content-Type: application/x-www-form-urlencoded" \
+--data-urlencode "username=admin" \
+--data-urlencode "password=${KEYCLOAK_ADMIN_PASSWORD}" \
+--data-urlencode "client_id=admin-cli" \
+--data-urlencode "grant_type=password" | uds zarf tools yq .access_token)
 echo "::add-mask::$KEYCLOAK_ADMIN_TOKEN"
 
+
 curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/users" \
-	--http1.1 \
-	--header "Content-Type: application/json" \
-	--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" \
-	--data "{
+--http1.1 \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" \
+--data "{
   \"username\": \"doug\",
   \"firstName\": \"Doug\",
   \"lastName\": \"Unicorn\",
@@ -35,23 +36,22 @@ curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/users" \
 }"
 
 CONDITIONAL_OTP_ID=$(curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/authentication/flows/Authentication/executions" \
-	--http1.1 \
-	--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" | uds zarf tools yq '.[] | select(.displayName == "Conditional OTP") | .id')
+--http1.1 \
+--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" | uds zarf tools yq '.[] | select(.displayName == "Conditional OTP") | .id')
 
 curl --location --request PUT "https://keycloak.admin.uds.dev/admin/realms/uds/authentication/flows/Authentication/executions" \
-	--http1.1 \
-	--header "Content-Type: application/json" \
-	--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" \
-	--data "{
+--http1.1 \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" \
+--data "{
 \"id\": \"${CONDITIONAL_OTP_ID}\",
 \"requirement\": \"DISABLED\"
 }"
 
-USER=$(
-	curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/users?user=doug" \
-		--http1.1 \
-		--header "Content-Type: application/json" \
-		--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}"
+USER=$(curl --location "https://keycloak.admin.uds.dev/admin/realms/uds/users?user=doug" \
+--http1.1 \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer ${KEYCLOAK_ADMIN_TOKEN}" \
 )
 
 echo "User: $USER"
