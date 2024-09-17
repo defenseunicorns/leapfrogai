40f78e0fcf39709277dc5d8feed765182f689131
gharvey <gato.harvey@defenseunicorns.com>
2024-09-17 14:21:57 -0700

    Updates all grpc code to protobuf 5.27.2

--- src/leapfrogai_sdk/name/name_pb2.py
+++ src/leapfrogai_sdk/name/name_pb2.py
@@ -3,7 +3,6 @@
 # source: leapfrogai_sdk/name/name.proto
 # Protobuf Python Version: 4.25.1
 """Generated protocol buffer code."""
-
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import symbol_database as _symbol_database
@@ -16,22 +15,16 @@ _sym_db = _symbol_database.Default()
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n\x1eleapfrogai_sdk/name/name.proto\x12\x04name\x1a\x1bgoogle/protobuf/empty.proto"\x1c\n\x0cNameResponse\x12\x0c\n\x04name\x18\x01 \x01(\t2A\n\x0bNameService\x12\x32\n\x04Name\x12\x16.google.protobuf.Empty\x1a\x12.name.NameResponseB7Z5github.com/defenseunicorns/leapfrogai/pkg/client/nameb\x06proto3'
-)
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1eleapfrogai_sdk/name/name.proto\x12\x04name\x1a\x1bgoogle/protobuf/empty.proto\"\x1c\n\x0cNameResponse\x12\x0c\n\x04name\x18\x01 \x01(\t2A\n\x0bNameService\x12\x32\n\x04Name\x12\x16.google.protobuf.Empty\x1a\x12.name.NameResponseB7Z5github.com/defenseunicorns/leapfrogai/pkg/client/nameb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(
-    DESCRIPTOR, "leapfrogai_sdk.name.name_pb2", _globals
-)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'leapfrogai_sdk.name.name_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
-    _globals["DESCRIPTOR"]._options = None
-    _globals[
-        "DESCRIPTOR"
-    ]._serialized_options = b"Z5github.com/defenseunicorns/leapfrogai/pkg/client/name"
-    _globals["_NAMERESPONSE"]._serialized_start = 69
-    _globals["_NAMERESPONSE"]._serialized_end = 97
-    _globals["_NAMESERVICE"]._serialized_start = 99
-    _globals["_NAMESERVICE"]._serialized_end = 164
+  _globals['DESCRIPTOR']._options = None
+  _globals['DESCRIPTOR']._serialized_options = b'Z5github.com/defenseunicorns/leapfrogai/pkg/client/name'
+  _globals['_NAMERESPONSE']._serialized_start=69
+  _globals['_NAMERESPONSE']._serialized_end=97
+  _globals['_NAMESERVICE']._serialized_start=99
+  _globals['_NAMESERVICE']._serialized_end=164
 # @@protoc_insertion_point(module_scope)
