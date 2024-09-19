24fe2fc33ef9cd321ab12f110b359c02ee59f608
gharvey <gato.harvey@defenseunicorns.com>
2024-09-18 18:20:11 -0700

    Revert "Test generating all protos as group"
    This reverts commit 4996121c1023817a4ae68bbcb07fc6cfdc26ffdf.

--- src/leapfrogai_sdk/chat/chat_pb2.pyi
+++ src/leapfrogai_sdk/chat/chat_pb2.pyi
@@ -2,7 +2,13 @@ from google.protobuf.internal import containers as _containers
 from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from typing import (
+    ClassVar as _ClassVar,
+    Iterable as _Iterable,
+    Mapping as _Mapping,
+    Optional as _Optional,
+    Union as _Union,
+)
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -12,6 +18,7 @@ class ChatRole(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     SYSTEM: _ClassVar[ChatRole]
     FUNCTION: _ClassVar[ChatRole]
     ASSISTANT: _ClassVar[ChatRole]
+
 USER: ChatRole
 SYSTEM: ChatRole
 FUNCTION: ChatRole
@@ -23,17 +30,44 @@ class ChatItem(_message.Message):
     CONTENT_FIELD_NUMBER: _ClassVar[int]
     role: ChatRole
     content: str
-    def __init__(self, role: _Optional[_Union[ChatRole, str]] = ..., content: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self,
+        role: _Optional[_Union[ChatRole, str]] = ...,
+        content: _Optional[str] = ...,
+    ) -> None: ...
 
 class ChatCompletionRequest(_message.Message):
-    __slots__ = ("chat_items", "max_new_tokens", "temperature", "top_k", "top_p", "do_sample", "n", "stop", "repetition_penalty", "presence_penalty", "frequency_penalty", "best_of", "logit_bias", "return_full_text", "truncate", "typical_p", "watermark", "seed", "user")
+    __slots__ = (
+        "chat_items",
+        "max_new_tokens",
+        "temperature",
+        "top_k",
+        "top_p",
+        "do_sample",
+        "n",
+        "stop",
+        "repetition_penalty",
+        "presence_penalty",
+        "frequency_penalty",
+        "best_of",
+        "logit_bias",
+        "return_full_text",
+        "truncate",
+        "typical_p",
+        "watermark",
+        "seed",
+        "user",
+    )
     class LogitBiasEntry(_message.Message):
         __slots__ = ("key", "value")
         KEY_FIELD_NUMBER: _ClassVar[int]
         VALUE_FIELD_NUMBER: _ClassVar[int]
         key: str
         value: int
-        def __init__(self, key: _Optional[str] = ..., value: _Optional[int] = ...) -> None: ...
+        def __init__(
+            self, key: _Optional[str] = ..., value: _Optional[int] = ...
+        ) -> None: ...
+
     CHAT_ITEMS_FIELD_NUMBER: _ClassVar[int]
     MAX_NEW_TOKENS_FIELD_NUMBER: _ClassVar[int]
     TEMPERATURE_FIELD_NUMBER: _ClassVar[int]
@@ -72,7 +106,28 @@ class ChatCompletionRequest(_message.Message):
     watermark: bool
     seed: int
     user: str
-    def __init__(self, chat_items: _Optional[_Iterable[_Union[ChatItem, _Mapping]]] = ..., max_new_tokens: _Optional[int] = ..., temperature: _Optional[float] = ..., top_k: _Optional[float] = ..., top_p: _Optional[float] = ..., do_sample: bool = ..., n: _Optional[int] = ..., stop: _Optional[_Iterable[str]] = ..., repetition_penalty: _Optional[float] = ..., presence_penalty: _Optional[float] = ..., frequency_penalty: _Optional[float] = ..., best_of: _Optional[str] = ..., logit_bias: _Optional[_Mapping[str, int]] = ..., return_full_text: bool = ..., truncate: _Optional[int] = ..., typical_p: _Optional[float] = ..., watermark: bool = ..., seed: _Optional[int] = ..., user: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self,
+        chat_items: _Optional[_Iterable[_Union[ChatItem, _Mapping]]] = ...,
+        max_new_tokens: _Optional[int] = ...,
+        temperature: _Optional[float] = ...,
+        top_k: _Optional[float] = ...,
+        top_p: _Optional[float] = ...,
+        do_sample: bool = ...,
+        n: _Optional[int] = ...,
+        stop: _Optional[_Iterable[str]] = ...,
+        repetition_penalty: _Optional[float] = ...,
+        presence_penalty: _Optional[float] = ...,
+        frequency_penalty: _Optional[float] = ...,
+        best_of: _Optional[str] = ...,
+        logit_bias: _Optional[_Mapping[str, int]] = ...,
+        return_full_text: bool = ...,
+        truncate: _Optional[int] = ...,
+        typical_p: _Optional[float] = ...,
+        watermark: bool = ...,
+        seed: _Optional[int] = ...,
+        user: _Optional[str] = ...,
+    ) -> None: ...
 
 class ChatCompletionChoice(_message.Message):
     __slots__ = ("index", "chat_item", "finish_reason")
@@ -82,7 +137,12 @@ class ChatCompletionChoice(_message.Message):
     index: int
     chat_item: ChatItem
     finish_reason: str
-    def __init__(self, index: _Optional[int] = ..., chat_item: _Optional[_Union[ChatItem, _Mapping]] = ..., finish_reason: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self,
+        index: _Optional[int] = ...,
+        chat_item: _Optional[_Union[ChatItem, _Mapping]] = ...,
+        finish_reason: _Optional[str] = ...,
+    ) -> None: ...
 
 class Usage(_message.Message):
     __slots__ = ("prompt_tokens", "completion_tokens", "total_tokens")
@@ -92,7 +152,12 @@ class Usage(_message.Message):
     prompt_tokens: int
     completion_tokens: int
     total_tokens: int
-    def __init__(self, prompt_tokens: _Optional[int] = ..., completion_tokens: _Optional[int] = ..., total_tokens: _Optional[int] = ...) -> None: ...
+    def __init__(
+        self,
+        prompt_tokens: _Optional[int] = ...,
+        completion_tokens: _Optional[int] = ...,
+        total_tokens: _Optional[int] = ...,
+    ) -> None: ...
 
 class ChatCompletionResponse(_message.Message):
     __slots__ = ("id", "object", "created", "choices", "usage")
@@ -106,4 +171,11 @@ class ChatCompletionResponse(_message.Message):
     created: int
     choices: _containers.RepeatedCompositeFieldContainer[ChatCompletionChoice]
     usage: Usage
-    def __init__(self, id: _Optional[str] = ..., object: _Optional[str] = ..., created: _Optional[int] = ..., choices: _Optional[_Iterable[_Union[ChatCompletionChoice, _Mapping]]] = ..., usage: _Optional[_Union[Usage, _Mapping]] = ...) -> None: ...
+    def __init__(
+        self,
+        id: _Optional[str] = ...,
+        object: _Optional[str] = ...,
+        created: _Optional[int] = ...,
+        choices: _Optional[_Iterable[_Union[ChatCompletionChoice, _Mapping]]] = ...,
+        usage: _Optional[_Union[Usage, _Mapping]] = ...,
+    ) -> None: ...
--- src/leapfrogai_sdk/embeddings/embeddings_pb2.py
+++ src/leapfrogai_sdk/embeddings/embeddings_pb2.py
@@ -3,6 +3,7 @@
 # source: leapfrogai_sdk/embeddings/embeddings.proto
 # Protobuf Python Version: 4.25.1
 """Generated protocol buffer code."""
+
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import symbol_database as _symbol_database
@@ -12,22 +13,28 @@ from google.protobuf.internal import builder as _builder
 _sym_db = _symbol_database.Default()
 
 
-
-
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n*leapfrogai_sdk/embeddings/embeddings.proto\x12\nembeddings\"\"\n\x10\x45mbeddingRequest\x12\x0e\n\x06inputs\x18\x01 \x03(\t\"\x1e\n\tEmbedding\x12\x11\n\tembedding\x18\x01 \x03(\x02\">\n\x11\x45mbeddingResponse\x12)\n\nembeddings\x18\x01 \x03(\x0b\x32\x15.embeddings.Embedding2c\n\x11\x45mbeddingsService\x12N\n\x0f\x43reateEmbedding\x12\x1c.embeddings.EmbeddingRequest\x1a\x1d.embeddings.EmbeddingResponseB=Z;github.com/defenseunicorns/leapfrogai/pkg/client/embeddingsb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
+    b'\n*leapfrogai_sdk/embeddings/embeddings.proto\x12\nembeddings""\n\x10\x45mbeddingRequest\x12\x0e\n\x06inputs\x18\x01 \x03(\t"\x1e\n\tEmbedding\x12\x11\n\tembedding\x18\x01 \x03(\x02">\n\x11\x45mbeddingResponse\x12)\n\nembeddings\x18\x01 \x03(\x0b\x32\x15.embeddings.Embedding2c\n\x11\x45mbeddingsService\x12N\n\x0f\x43reateEmbedding\x12\x1c.embeddings.EmbeddingRequest\x1a\x1d.embeddings.EmbeddingResponseB=Z;github.com/defenseunicorns/leapfrogai/pkg/client/embeddingsb\x06proto3'
+)
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'leapfrogai_sdk.embeddings.embeddings_pb2', _globals)
+_builder.BuildTopDescriptorsAndMessages(
+    DESCRIPTOR, "leapfrogai_sdk.embeddings.embeddings_pb2", _globals
+)
 if _descriptor._USE_C_DESCRIPTORS == False:
-  _globals['DESCRIPTOR']._options = None
-  _globals['DESCRIPTOR']._serialized_options = b'Z;github.com/defenseunicorns/leapfrogai/pkg/client/embeddings'
-  _globals['_EMBEDDINGREQUEST']._serialized_start=58
-  _globals['_EMBEDDINGREQUEST']._serialized_end=92
-  _globals['_EMBEDDING']._serialized_start=94
-  _globals['_EMBEDDING']._serialized_end=124
-  _globals['_EMBEDDINGRESPONSE']._serialized_start=126
-  _globals['_EMBEDDINGRESPONSE']._serialized_end=188
-  _globals['_EMBEDDINGSSERVICE']._serialized_start=190
-  _globals['_EMBEDDINGSSERVICE']._serialized_end=289
+    _globals["DESCRIPTOR"]._options = None
+    _globals[
+        "DESCRIPTOR"
+    ]._serialized_options = (
+        b"Z;github.com/defenseunicorns/leapfrogai/pkg/client/embeddings"
+    )
+    _globals["_EMBEDDINGREQUEST"]._serialized_start = 58
+    _globals["_EMBEDDINGREQUEST"]._serialized_end = 92
+    _globals["_EMBEDDING"]._serialized_start = 94
+    _globals["_EMBEDDING"]._serialized_end = 124
+    _globals["_EMBEDDINGRESPONSE"]._serialized_start = 126
+    _globals["_EMBEDDINGRESPONSE"]._serialized_end = 188
+    _globals["_EMBEDDINGSSERVICE"]._serialized_start = 190
+    _globals["_EMBEDDINGSSERVICE"]._serialized_end = 289
 # @@protoc_insertion_point(module_scope)
