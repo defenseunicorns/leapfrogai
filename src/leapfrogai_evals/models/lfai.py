7cf3698e8392090e3085943e8a0b7df5859a6c18
John Alling <john.alling@defenseunicorns.com>
2024-09-19 11:48:05 -0400

    add lfai model and move dirs

---
+++ src/leapfrogai_evals/models/lfai.py
@@ -0,0 +1,64 @@
+import os
+
+import openai
+from pydantic import BaseModel
+from deepeval.models.base_model import DeepEvalBaseLLM
+import asyncio
+from typing import Optional
+
+
+class LFAI_Model(DeepEvalBaseLLM):
+    """
+    A DeepEval LLM class that utilizes the LeapfrogAI API to prompt certain benchmarks
+
+    Do not use this particular class as an LLM judge.
+    This model class is needed to run generation-focused benchmarks, not be used as a judge.
+    Use Claude Sonnet or another larger model for LLM-as-judge evaluations
+    """
+
+    def __init__(
+        self,
+        api_key: Optional[str] = None,
+        base_url: Optional[str] = None,
+        model: str = "vllm",
+    ):
+        self.model = model
+        self.api_key = api_key or os.getenv("LEAPFROGAI_API_KEY")
+        self.base_url = base_url or os.getenv("LEAPFROGAI_API_URL")
+        self.client = openai.OpenAI(api_key=self.api_key, base_url=self.base_url)
+
+    def load_model(self):
+        """Returns the current model selected"""
+        return self.model
+
+    def generate(
+        self,
+        prompt: str,
+        max_tokens: int = 1024,
+        temperature: float = 0.75,
+        top_p: float = 1.0,
+        stop: str = "</s>",
+    ) -> str:
+        """Generates a response from LeapfrogAI API using the OpenAI SDK"""
+        response = self.client.chat.completions.create(
+            temperature=temperature,
+            model=self.model,
+            max_tokens=max_tokens,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            top_p=top_p,
+            stop=stop,
+        )
+        return response.choices[0].message.content
+
+    async def a_generate(self, prompt: str, *args, **kwargs) -> BaseModel:
+        """Async implementation of the generate function"""
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(None, self.generate, prompt, *args, **kwargs)
+
+    def get_model_name(self):
+        return f"LeapfrogAI {self.model}"
