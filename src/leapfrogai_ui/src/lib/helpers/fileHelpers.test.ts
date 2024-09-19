991c6e436d4281cfd75b798795ad741fa05ad89f
Andrew Risse <andrewrisse@gmail.com>
2024-09-19 11:32:46 -0600

    merge bug fixes from summarization branch

--- src/leapfrogai_ui/src/lib/helpers/fileHelpers.test.ts
+++ src/leapfrogai_ui/src/lib/helpers/fileHelpers.test.ts
@@ -1,15 +1,8 @@
 import { faker } from '@faker-js/faker';
 import type { FileMetadata } from '$lib/types/files';
-import { removeFilesUntilUnderLimit } from '$helpers/fileHelpers';
+import { removeFilesUntilUnderLimit, updateFileMetadata } from '$helpers/fileHelpers';
 import { FILE_CONTEXT_TOO_LARGE_ERROR_MSG } from '$constants/errors';
-
-const getMockFileMetadata = (text = faker.word.noun()): FileMetadata => ({
-  id: faker.string.uuid().substring(0, 8),
-  name: 'fake-file.pdf',
-  type: 'application/pdf',
-  text,
-  status: 'complete'
-});
+import { getMockFileMetadata } from '$testUtils/fakeData';
 
 describe('removeFilesUntilUnderLimit', () => {
   test('removeFilesUntilUnderLimit should remove the largest file until total size is under the max limit', () => {
@@ -19,9 +12,9 @@ describe('removeFilesUntilUnderLimit', () => {
     const text2 = faker.word.words(150);
     const text3 = faker.word.words(200);
     const files = [
-      getMockFileMetadata(text1),
-      getMockFileMetadata(text2),
-      getMockFileMetadata(text3)
+      getMockFileMetadata({ text: text1 }),
+      getMockFileMetadata({ text: text2 }),
+      getMockFileMetadata({ text: text3 })
     ];
 
     // Files 2 and 3 will have different length once their text is removed and they are set to error status
@@ -74,7 +67,7 @@ describe('removeFilesUntilUnderLimit', () => {
     const text1 = faker.word.words(10);
     const text2 = faker.word.words(20);
 
-    const files = [getMockFileMetadata(text1), getMockFileMetadata(text2)];
+    const files = [getMockFileMetadata({ text: text1 }), getMockFileMetadata({ text: text2 })];
 
     // Assume a limit of 50 characters
     const maxLimit = 10000000000000;
@@ -91,7 +84,7 @@ describe('removeFilesUntilUnderLimit', () => {
     const text1 = faker.word.words(10);
     const text2 = faker.word.words(20);
 
-    const files = [getMockFileMetadata(text1), getMockFileMetadata(text2)];
+    const files = [getMockFileMetadata({ text: text1 }), getMockFileMetadata({ text: text2 })];
 
     // Assume a limit of 50 characters
     const maxLimit = 5;
@@ -104,5 +97,50 @@ describe('removeFilesUntilUnderLimit', () => {
     expect(files[1].text).toBe('');
     expect(files[0].status).toBe('error');
     expect(files[1].status).toBe('error');
+  });
+});
+
+describe('updateFileMetadata with order preservation', () => {
+  it('should update existing files, add new files, and preserve the original order of old metadata', () => {
+    const file1 = getMockFileMetadata({ status: 'complete' });
+    const file2 = getMockFileMetadata({ status: 'uploading' });
+    const file3 = getMockFileMetadata({ status: 'complete' });
+    const file4 = getMockFileMetadata({ status: 'complete' });
+
+    // 3 files
+    const oldMetadata: FileMetadata[] = [file1, file2, file3];
+
+    // updated file, and new file
+    const newMetadata: FileMetadata[] = [file1, { ...file2, status: 'complete' }, file3, file4];
+
+    const result = updateFileMetadata(oldMetadata, newMetadata);
+
+    expect(result).toHaveLength(4);
+
+    // Check if the order is preserved and one file was updated and new one added
+
+    expect(result[0]).toEqual(file1);
+    expect(result[1]).toEqual({
+      ...file2,
+      status: 'complete'
+    });
+
+    expect(result[2]).toEqual(file3);
+    expect(result[3]).toEqual(file4);
+  });
+
+  it('should return new metadata if old metadata is empty', () => {
+    const oldMetadata: FileMetadata[] = [];
+    const newMetadata: FileMetadata[] = [getMockFileMetadata()];
+    const result = updateFileMetadata(oldMetadata, newMetadata);
+    expect(result).toHaveLength(1);
+    expect(result).toEqual(newMetadata);
+  });
+
+  it('should keep old metadata if no new metadata is provided', () => {
+    const oldMetadata: FileMetadata[] = [getMockFileMetadata()];
+    const newMetadata: FileMetadata[] = [];
+    const result = updateFileMetadata(oldMetadata, newMetadata);
+    expect(result).toEqual(oldMetadata);
   });
 });
