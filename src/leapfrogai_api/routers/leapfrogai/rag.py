f14e22d915c6dc64cd8924ca98c7b913be44cbd7
gharvey <gato.harvey@defenseunicorns.com>
2024-09-20 15:33:17 -0700

    Adds endpoint to check current rag configuraiton

--- src/leapfrogai_api/routers/leapfrogai/rag.py
+++ src/leapfrogai_api/routers/leapfrogai/rag.py
@@ -1,8 +1,8 @@
 """LeapfrogAI endpoints for RAG."""
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from leapfrogai_api.typedef.rag.rag_types import Configuration
-from leapfrogai_api.routers.supabase_session import Session
+from leapfrogai_api.routers.supabase_session import Session, get_session
 
 router = APIRouter(prefix="/leapfrogai/v1/rag", tags=["leapfrogai/rag"])
 
@@ -19,3 +19,17 @@ async def configure(session: Session, configuration: Configuration):
 
     # We set the class variable to update the configuration globally
     Configuration.enable_reranking = configuration.enable_reranking
+
+
+@router.get("/configuration")
+async def get_configuration(session: Session = Depends(get_session)):
+    """
+    Retrieves the current RAG configuration.
+
+    Args:
+        session (Session): The database session.
+
+    Returns:
+        Configuration: The current RAG configuration.
+    """
+    return Configuration(enable_reranking=Configuration.enable_reranking)
