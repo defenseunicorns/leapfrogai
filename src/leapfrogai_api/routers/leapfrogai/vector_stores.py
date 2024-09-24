d19be308ea20a61b258ac05479f8b24f2c65cf56
gharvey <gato.harvey@defenseunicorns.com>
2024-09-24 15:24:16 -0700

    Adds the model config to the search endpoint so that it can be passed down

--- src/leapfrogai_api/routers/leapfrogai/vector_stores.py
+++ src/leapfrogai_api/routers/leapfrogai/vector_stores.py
@@ -1,10 +1,13 @@
 """LeapfrogAI endpoints for RAG."""
 
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 from leapfrogai_api.backend.rag.query import QueryService
 from leapfrogai_api.typedef.vectorstores import SearchResponse
 from leapfrogai_api.routers.supabase_session import Session
 from leapfrogai_api.backend.constants import TOP_K
+from leapfrogai_api.utils import Config, get_model_config
 
 router = APIRouter(
     prefix="/leapfrogai/v1/vector_stores", tags=["leapfrogai/vector_stores"]
@@ -14,6 +17,7 @@ router = APIRouter(
 @router.post("/search")
 async def search(
     session: Session,
+    model_config: Annotated[Config, Depends(get_model_config)],
     query: str,
     vector_store_id: str,
     k: int = TOP_K,
@@ -23,6 +27,7 @@ async def search(
 
     Args:
         session (Session): The database session.
+        model_config (Config): The current model configuration.
         query (str): The input query string.
         vector_store_id (str): The ID of the vector store.
         k (int, optional): The number of results to retrieve.
@@ -32,7 +37,5 @@ async def search(
     """
     query_service = QueryService(db=session)
     return await query_service.query_rag(
-        query=query,
-        vector_store_id=vector_store_id,
-        k=k,
+        query=query, vector_store_id=vector_store_id, k=k, model_config=model_config
     )
