8e68b8bfbf9566e4e9a0639187b935c9526c4339
John Alling <44934218+jalling97@users.noreply.github.com>
2024-09-25 08:15:33 -0400

    feat: 1049 add standard evaluation benchmarks to lfai evals (#1078)
    * refactor eval structure
    * add MMLU benchmark
    * add HumanEval benchmarks
    * add DeepEval compatible LLM class using LFAI
    * update evals README
    * upgrade DeepEval to v1.3.0

---
+++ src/leapfrogai_evals/metrics/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+# ruff: noqa: F401
+
+from leapfrogai_evals.metrics.annotation_relevancy import AnnotationRelevancyMetric
+from leapfrogai_evals.metrics.correctness import CorrectnessMetric
+from leapfrogai_evals.metrics.niah_metrics import NIAH_Response, NIAH_Retrieval
