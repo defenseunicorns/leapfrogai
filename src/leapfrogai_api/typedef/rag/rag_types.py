f46b599c73c27614bc45e3495a84bda403a59c00
gharvey <gato.harvey@defenseunicorns.com>
2024-09-25 12:15:32 -0700

    Removes transformers options from rerankers

--- src/leapfrogai_api/typedef/rag/rag_types.py
+++ src/leapfrogai_api/typedef/rag/rag_types.py
@@ -9,6 +9,9 @@ class Configuration(BaseModel):
     # This is a class variable, shared by all instances of Configuration
     # It sets a default value, but doesn't create an instance variable
     enable_reranking: ClassVar[bool] = False
+    # More model info can be found here:
+    # https://github.com/AnswerDotAI/rerankers?tab=readme-ov-file
+    # https://pypi.org/project/rerankers/
     ranking_model: ClassVar[str] = "flashrank"
 
     # Note: Pydantic will not create an instance variable for ClassVar fields
@@ -18,11 +21,17 @@ class Configuration(BaseModel):
 class ConfigurationPayload(BaseModel):
     """Response for RAG configuration."""
 
+    # Singleton instance of this class
+    instance: ClassVar[any] = None
+
     # This is an instance variable, specific to each ConfigurationPayload object
     # It will be included in the JSON output when the model is serialized
     enable_reranking: bool = Field(
         default=False, description="Enables reranking for RAG queries"
     )
+    # More model info can be found here:
+    # https://github.com/AnswerDotAI/rerankers?tab=readme-ov-file
+    # https://pypi.org/project/rerankers/
     ranking_model: str = Field(
         default="flashrank",
         description="What model to use for reranking",
