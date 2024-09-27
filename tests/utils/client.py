d8bf65cb797902af3a55cea297a912bc507e301f
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-09-27 11:27:50 -0400

    spiff up client

--- tests/utils/client.py
+++ tests/utils/client.py
@@ -6,48 +6,104 @@ from requests import Response
 import requests
 
 
-def get_leapfrogai_model():
-    try:
-        return os.getenv("LEAPFROGAI_MODEL", "vllm")
-    except KeyError:
-        raise ValueError("LEAPFROGAI_MODEL not set")
+def get_leapfrogai_model() -> str:
+    """Get the model to use for LeapfrogAI.
+
+    Returns:
+        str: The model to use for LeapfrogAI. (default: "vllm")
+    """
+
+    return os.getenv("LEAPFROGAI_MODEL", "vllm")
 
 
-def get_openai_model():
-    try:
-        return os.getenv("OPENAI_MODEL", "gpt-4o-mini")
-    except KeyError:
-        raise ValueError("OPENAI_MODEL not set")
+def get_openai_key() -> str:
+    """Get the API key for OpenAI.
+
+    Returns:
+        str: The API key for OpenAI.
+
+    Raises:
+        ValueError: If OPENAI_API_KEY is not set.
+    """
+
+    api_key = os.getenv("OPENAI_API_KEY")
+    if api_key is None:
+        raise ValueError("OPENAI_API_KEY not set")
+
+    return api_key
 
 
-def get_leapfrogai_api_key():
-    try:
-        return os.getenv("LEAPFROGAI_API_KEY") or os.getenv("SUPABASE_USER_JWT")
-    except KeyError:
+def get_openai_model() -> str:
+    """Get the model to use for OpenAI.
+
+    Returns:
+        str: The model to use for OpenAI. (default: "gpt-4o-mini")
+    """
+
+    return os.getenv("OPENAI_MODEL", "gpt-4o-mini")
+
+
+def get_leapfrogai_api_key() -> str:
+    """Get the API key for the LeapfrogAI API.
+
+    Set via the LEAPFROGAI_API_KEY environment variable or the SUPABASE_USER_JWT environment variable in that order.
+
+    Returns:
+        str: The API key for the LeapfrogAI API.
+    Raises:
+        ValueError: If LEAPFROGAI_API_KEY or SUPABASE_USER_JWT is not set.
+    """
+
+    api_key = os.getenv("LEAPFROGAI_API_KEY") or os.getenv("SUPABASE_USER_JWT")
+
+    if api_key is None:
         raise ValueError("LEAPFROGAI_API_KEY or SUPABASE_USER_JWT not set")
 
-
-def get_leapfrogai_api_url():
-    try:
-        return os.getenv(
-            "LEAPFROGAI_API_URL", "https://leapfrogai-api.uds.dev/openai/v1"
-        )
-    except KeyError:
-        raise ValueError("LEAPFROGAI_API_URL not set")
+    return api_key
 
 
-def get_leapfrogai_api_url_other():
-    try:
-        return os.getenv("LEAPFROGAI_API_URL_OTHER", "https://leapfrogai-api.uds.dev")
-    except KeyError:
-        raise ValueError("LEAPFROGAI_API_URL_OTHER not set")
+def get_leapfrogai_api_url() -> str:
+    """Get the URL for the LeapfrogAI API.
+
+    Returns:
+        str: The URL for the LeapfrogAI API. (default: "https://leapfrogai-api.uds.dev/openai/v1")
+    """
+
+    return os.getenv("LEAPFROGAI_API_URL", "https://leapfrogai-api.uds.dev/openai/v1")
 
 
-def openai_client():
-    return OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+def get_leapfrogai_api_url_base() -> str:
+    """Get the base URL for the LeapfrogAI API.
+
+    Set via the LEAPFRAGAI_API_URL environment variable.
+
+    If LEAPFRAGAI_API_URL is set to "https://leapfrogai-api.uds.dev/openai/v1", this will trim off the "/openai/v1" part.
+
+    Returns:
+        str: The base URL for the LeapfrogAI API. (default: "https://leapfrogai-api.uds.dev")
+    """
+
+    url = os.getenv("LEAPFROGAI_API_URL", "https://leapfrogai-api.uds.dev")
+    if url.endswith("/openai/v1"):
+        return url[:-9]
+    return url
 
 
-def leapfrogai_client():
+def openai_client() -> OpenAI:
+    """Create an OpenAI client using the OPENAI_API_KEY.
+
+    returns:
+        OpenAI: An OpenAI client.
+    """
+    return OpenAI(api_key=get_openai_key())
+
+
+def leapfrogai_client() -> OpenAI:
+    """Create an OpenAI client using the LEAPFROGAI_API_URL and LEAPFROGAI_API_KEY or SUPABASE_USER_JWT.
+
+    returns:
+        OpenAI: An OpenAI client.
+    """
     return OpenAI(
         base_url=get_leapfrogai_api_url(),
         api_key=get_leapfrogai_api_key(),
@@ -55,6 +111,8 @@ def leapfrogai_client():
 
 
 class ClientConfig:
+    """Configuration for a client that is OpenAI compliant."""
+
     client: OpenAI
     model: str
 
@@ -74,10 +132,16 @@ def client_config_factory(client_name: str) -> ClientConfig:
 
 
 class LeapfrogAIClient:
-    """Client for handling queries in the LeapfrogAI namespace that are not handled by the OpenAI SDK."""
+    """Client for handling queries in the LeapfrogAI namespace that are not handled by the OpenAI SDK.
+
+    Wraps the requests library to make HTTP requests to the LeapfrogAI API.
+
+    Raises:
+        requests.HTTPError: If the response status code is not a 2xx status code.
+    """
 
     def __init__(self, base_url: str | None = None, api_key: str | None = None):
-        self.base_url = base_url or get_leapfrogai_api_url_other()
+        self.base_url = base_url or get_leapfrogai_api_url_base()
         self.api_key = api_key or get_leapfrogai_api_key()
         self.headers = {
             "accept": "application/json",
